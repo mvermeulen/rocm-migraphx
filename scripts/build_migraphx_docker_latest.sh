3468b3e877732e41525fd7682432f2f4be607f44
Mike Vermeulen <mike@vermeulen.com>
2020-03-08 07:36:32 -0500

    Fix up build scripts.

--- scripts/build_migraphx_docker_latest.sh
+++ scripts/build_migraphx_docker_latest.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DATESTAMP=`date '+%Y%m%d'`
+BASE=rocm-migraphx:3.1
 if [ `id -u` != 0 ]; then
     echo script should be run as root
     exit 0
 fi
 cd ../dockerfiles
-docker build -t rocm-migraphx:${DATESTAMP} -f ../dockerfiles/dockerfile-daily .
+docker build -t rocm-migraphx:${DATESTAMP} --build-arg DOCKERBASE=${BASE} -f ../dockerfiles/dockerfile-daily .
