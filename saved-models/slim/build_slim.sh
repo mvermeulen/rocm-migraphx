f15b9fc8bd502a64ccb4ddb48102df7d244745be
Mike Vermeulen <mike@vermeulen.com>
2020-03-12 15:12:04 -0500

    Update the build_slim script.

--- saved-models/slim/build_slim.sh
+++ saved-models/slim/build_slim.sh
@@ -24,4 +24,29 @@ download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz
 download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz                         
 storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz        
 storage.googleapis.com/download.tensorflow.org/models/nasnet-a_large_04_10_2017.tar.gz
-FILELIST   
+FILELIST
+
+# InceptionV3
+tar xf inception_v3_2016_08_28.tar.gz
+
+# InceptionV3
+tar xf inception_v4_2016_09_09.tar.gz
+python3 ./models/research/slim/export_inference_graph.py --model_name=inception_v4 --output_file=inception_v4_model1.pb
+python3 ./models/research/slim/export_inference_graph.py --model_name=inception_v4 --output_file=inception_v4_model1.pb
+python3 ./models/research/slim/export_inference_graph.py --model_name=inception_v4 --batch_size=16 --output_file=inception_v4_model16.pb
+
+
+# Mobilenet
+tar xf mobilenet_v2_1.4_224.tgz
+
+# NASNetALarge
+tar xf nasnet-a_large_04_10_2017.tar.gz
+
+# ResNet V1
+tar xf resnet_v1_50_2016_08_28.tar.gz
+
+# ResNet V2
+tar xf resnet_v2_50_2017_04_14.tar.gz
+
+# vgg 16
+tar xf vgg_16_2016_08_28.tar.gz
