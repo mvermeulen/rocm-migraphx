612b0f073cb62fa3fa42dc1b6d104a6a2510322f
Mike Vermeulen <mike@vermeulen.com>
2020-03-15 20:58:55 -0500

    Update slim script to export all the models.

--- saved-models/slim/build_slim.sh
+++ saved-models/slim/build_slim.sh
@@ -24,8 +24,9 @@ download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz
 download.tensorflow.org/models/resnet_v2_50_2017_04_14.tar.gz                   
 download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz                   
 download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz                   
-download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz                         
-storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz        
+download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
+download.tensorflow.org/models/mobilenet_v1_2018_02_22/mobilenet_v1_1.0_224.tgz
+storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz
 storage.googleapis.com/download.tensorflow.org/models/nasnet-a_large_04_10_2017.tar.gz
 FILELIST
 
@@ -84,36 +85,140 @@ if [ ! -f inceptionv4_i1.pb -o ! -f inceptionv4_i16.pb ]; then
 fi
 
 # Mobilenet
+# V1 exports, but problems exporting V2
 if [ ! -f mobilenet_i1.pb -o ! -f mobilenet_i64.pb ]; then
     if [ -d mobilenet ]; then
 	rm -rf mobilenet
     fi
     mkdir mobilenet
     cd mobilenet
+    tar xf ../mobilenet_v1_1.0_224.tgz
     tar xf ../mobilenet_v2_1.4_224.tgz
-    python3 ${EXPORT_GRAPH} --model_name=mobilenet_v2 --output_file=./mobilenetv2_model1.pb --batch_size=1
-    python3 ${EXPORT_GRAPH} --model_name=mobilenet_v2 --output_file=./mobilenetv2_model64.pb --batch_size=64
-    ${SUMMARIZE_GRAPH} --in_graph=mobilenetv2_model1.pb > ./mobilenetv2_model1.sum
-    ${SUMMARIZE_GRAPH} --in_graph=mobilenetv2_model64.pb > ./mobilenetv2_model32.sum
+    python3 ${EXPORT_GRAPH} --model_name=mobilenet_v1 --output_file=./mobilenetv1_model1.pb --batch_size=1
+    python3 ${EXPORT_GRAPH} --model_name=mobilenet_v1 --output_file=./mobilenetv1_model64.pb --batch_size=64
+#    python3 ${EXPORT_GRAPH} --model_name=mobilenet_v2 --output_file=./mobilenetv2_model1.pb --batch_size=1
+#    python3 ${EXPORT_GRAPH} --model_name=mobilenet_v2 --output_file=./mobilenetv2_model64.pb --batch_size=64
+    ${SUMMARIZE_GRAPH} --in_graph=mobilenetv1_model1.pb > ./mobilenetv1_model1.sum
+    ${SUMMARIZE_GRAPH} --in_graph=mobilenetv1_model64.pb > ./mobilenetv1_model64.sum
     ${FREEZE_GRAPH} \
-	--input_graph=./mobilenetv2_model1.pb \
+	--input_graph=./mobilenetv1_model1.pb \
 	--input_binary=true \
-	--input_checkpoint=./mobilenet_v2_1.4_224.ckpt \
-	--output_node_names=MobilenetV2/Predictions/Reshape_1 \
+	--input_checkpoint=./mobilenet_v1_1.0_224.ckpt \
+	--output_node_names=MobilenetV1/Predictions/Reshape_1 \
 	--output_graph=../mobilenet_i1.pb
     ${FREEZE_GRAPH} \
-	--input_graph=./mobilenetv2_model64.pb \
+	--input_graph=./mobilenetv1_model64.pb \
 	--input_binary=true \
-	--input_checkpoint=./mobilenet_v2_1.4_224.ckpt \
-	--output_node_names=MobilenetV2/Predictions/Reshape_1 \
+	--input_checkpoint=./mobilenet_v1_1.0_224.ckpt \
+	--output_node_names=MobilenetV1/Predictions/Reshape_1 \
 	--output_graph=../mobilenet_i64.pb
     cd ..
 fi
 
 # NASNetALarge
+if [ ! -f nasnet_i1.pb -o ! -f nasnet_i16.pb ]; then
+    if [ -d nasnet ]; then
+	rm -rf nasnet
+    fi
+    mkdir nasnet
+    cd nasnet
+    tar xf ../nasnet-a_large_04_10_2017.tar.gz
+    python3 ${EXPORT_GRAPH} --model_name=nasnet_large --output_file=./nasnet_model1.pb --batch_size=1
+    python3 ${EXPORT_GRAPH} --model_name=nasnet_large --output_file=./nasnet_model16.pb --batch_size=16
+    ${SUMMARIZE_GRAPH} --in_graph=nasnet_model1.pb > ./nasnet_model1.sum
+    ${SUMMARIZE_GRAPH} --in_graph=nasnet_model16.pb > ./nasnet_model16.sum
+    ${FREEZE_GRAPH} \
+	--input_graph=./nasnet_model1.pb \
+	--input_binary=true \
+	--input_checkpoint=./model.ckpt \
+	--output_node_names=final_layer/predictions \
+	--output_graph=../nasnet_i1.pb
+    ${FREEZE_GRAPH} \
+	--input_graph=./nasnet_model16.pb \
+	--input_binary=true \
+	--input_checkpoint=./model.ckpt \
+	--output_node_names=final_layer/predictions \
+	--output_graph=../nasnet_i16.pb
+    cd ..
+fi
 
 # ResNet V1
+if [ ! -f resnet50v1_i1.pb -o ! -f resnet50v1_i64.pb ]; then
+    if [ -d resnet50v1 ]; then
+	rm -rf resnet50v1
+    fi
+    mkdir resnet50v1
+    cd resnet50v1
+    tar xf ../resnet_v1_50_2016_08_28.tar.gz
+    python3 ${EXPORT_GRAPH} --model_name=resnet_v1_50 --output_file=./resnet50v1_model1.pb --batch_size=1 --labels_offset=1
+    python3 ${EXPORT_GRAPH} --model_name=resnet_v1_50 --output_file=./resnet50v1_model64.pb --batch_size=64 --labels_offset=1
+    ${SUMMARIZE_GRAPH} --in_graph=resnet50v1_model1.pb > ./resnet50v1_model1.sum
+    ${SUMMARIZE_GRAPH} --in_graph=resnet50v1_model64.pb > ./resnet50v1_model64.sum
+    ${FREEZE_GRAPH} \
+	--input_graph=./resnet50v1_model1.pb \
+	--input_binary=true \
+	--input_checkpoint=./resnet_v1_50.ckpt \
+	--output_node_names=resnet_v1_50/predictions/Reshape_1 \
+	--output_graph=../resnet50v1_i1.pb
+    ${FREEZE_GRAPH} \
+	--input_graph=./resnet50v1_model64.pb \
+	--input_binary=true \
+	--input_checkpoint=./resnet_v1_50.ckpt \
+	--output_node_names=resnet_v1_50/predictions/Reshape_1 \
+	--output_graph=../resnet50v1_i64.pb
+    cd ..
+fi
 
 # ResNet V2
+if [ ! -f resnet50v2_i1.pb -o ! -f resnet50v2_i64.pb ]; then
+    if [ -d resnet50v2 ]; then
+	rm -rf resnet50v2
+    fi
+    mkdir resnet50v2
+    cd resnet50v2
+    tar xf ../resnet_v2_50_2017_04_14.tar.gz
+    python3 ${EXPORT_GRAPH} --model_name=resnet_v2_50 --output_file=./resnet50v2_model1.pb --batch_size=1
+    python3 ${EXPORT_GRAPH} --model_name=resnet_v2_50 --output_file=./resnet50v2_model64.pb --batch_size=64
+    ${SUMMARIZE_GRAPH} --in_graph=resnet50v2_model1.pb > ./resnet50v2_model1.sum
+    ${SUMMARIZE_GRAPH} --in_graph=resnet50v2_model64.pb > ./resnet50v2_model64.sum
+    ${FREEZE_GRAPH} \
+	--input_graph=./resnet50v2_model1.pb \
+	--input_binary=true \
+	--input_checkpoint=./resnet_v2_50.ckpt \
+	--output_node_names=resnet_v2_50/predictions/Reshape_1 \
+	--output_graph=../resnet50v2_i1.pb
+    ${FREEZE_GRAPH} \
+	--input_graph=./resnet50v2_model64.pb \
+	--input_binary=true \
+	--input_checkpoint=./resnet_v2_50.ckpt \
+	--output_node_names=resnet_v2_50/predictions/Reshape_1 \
+	--output_graph=../resnet50v2_i64.pb
+    cd ..
+fi
 
 # vgg 16
+if [ ! -f vgg16_i1.pb -o ! -f vgg16_i16.pb ]; then
+    if [ -d vgg16 ]; then
+	rm -rf vgg16
+    fi
+    mkdir vgg16
+    cd vgg16
+    tar xf ../vgg_16_2016_08_28.tar.gz
+    python3 ${EXPORT_GRAPH} --model_name=vgg_16 --output_file=./vgg16_model1.pb --batch_size=1 --labels_offset=1
+    python3 ${EXPORT_GRAPH} --model_name=vgg_16 --output_file=./vgg16_model16.pb --batch_size=16 --labels_offset=1
+    ${SUMMARIZE_GRAPH} --in_graph=vgg16_model1.pb > ./vgg16_model1.sum
+    ${SUMMARIZE_GRAPH} --in_graph=vgg16_model16.pb > ./vgg16_model16.sum
+    ${FREEZE_GRAPH} \
+	--input_graph=./vgg16_model1.pb \
+	--input_binary=true \
+	--input_checkpoint=./vgg_16.ckpt \
+	--output_node_names=vgg_16/fc8/squeezed \
+	--output_graph=../vgg16_i1.pb
+    ${FREEZE_GRAPH} \
+	--input_graph=./vgg16_model16.pb \
+	--input_binary=true \
+	--input_checkpoint=./vgg_16.ckpt \
+	--output_node_names=vgg_16/fc8/squeezed \
+	--output_graph=../vgg16_i16.pb
+    cd ..
+fi
