a96e9e50c5d4b152814f56553ce4b90dd89f9eb0
Mike Vermeulen <mike@vermeulen.com>
2020-03-17 21:07:39 -0500

    Add test runs.

--- scripts/run_perf.sh
+++ scripts/run_perf.sh
@@ -18,7 +18,7 @@ do
     fi
     $DRIVER perf $extra $SAVED_MODELS/$savefile > ${tag}.out 2> ${tag}.err
     time=`grep 'Total time' ${tag}.out | awk '{ print $3 }' | sed s/ms//g` >/dev/null 2>&1
-    echo $tag,$batch,$time | tee results.csv
+    echo $tag,$batch,$time | tee -a results.csv
 done <<MODELLIST
 torchvision-resnet50         64 torchvision/resnet50i64.onnx
 torchvision-resnet50_fp16    64 torchvision/resnet50i64.onnx --fp16
@@ -34,7 +34,19 @@ cadene-inceptionv4           16 cadene/inceptionv4i16.onnx
 cadene-resnext64x4           16 cadene/resnext101_64x4di16.onnx
 torchvision-vgg16            16 torchvision/vgg16i16.onnx
 torchvision-vgg16_fp16       16 torchvision/vgg16i16.onnx --fp16
-
+bert-mrpc-onnx                8 huggingface-transformers/bert_mrpc8.onnx
+bert-mrpc-tf                  1 tf-misc/bert-mrpc1.pb
+slim-mobilenet               64 slim/mobilenet_i64.pb
+slim-nasnetalarge            16 slim/nasnet_i16.pb
+slim-resnet50v2              64 slim/resnet50v2_i64.pb
 MODELLIST
+
+MIGX=${MIGX:="../tools/migx/build/migx"}
+${MIGX} --zero_input --onnx $SAVED_MODELS/pytorch-examples/wlang_gru.onnx --perf_report --argname=input.1 > wlang_gru.out
+time=`grep 'Total time' wlang_gru.out | awk '{ print $3 }' | sed s/ms//g` >/dev/null 2>&1
+echo "pytorchexamples-wlang-gru,1,$time" |  | tee -a results.csv
+${MIGX} --zero_input --onnx $SAVED_MODELS/pytorch-examples/wlang_lstm.onnx --perf_report --argname=input.1 > wlang.lstm.out
+time=`grep 'Total time' wlang_lstm.out | awk '{ print $3 }' | sed s/ms//g` >/dev/null 2>&1
+echo "pytorchexamples-wlang-lstm,1,$time" |  | tee -a results.csv
 
 
