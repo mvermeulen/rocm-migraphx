2e70e124a5e7336fa87243e71b4a8d1067bf312b
Mike Vermeulen <mike@vermeulen.com>
2020-03-24 11:03:15 -0500

    Remove build directory before rebuilding.

--- tools/migx/build_migx.sh
+++ tools/migx/build_migx.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
 # script to (re)build migx driver
-if [ ! -d build ]; then
-    mkdir build
+if [ -d build ]; then
+    rm -rf build
 fi
+mkdir build
 cd build
 env CXXFLAGS=-v cmake ..
 make
