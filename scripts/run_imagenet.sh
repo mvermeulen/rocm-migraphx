de6b1ea5c40316794b061636ca7f94010c65fd3a
Mike Vermeulen <mike@vermeulen.com>
2020-03-31 15:00:58 -0500

    Put results in a testdir.

--- scripts/run_imagenet.sh
+++ scripts/run_imagenet.sh
@@ -2,7 +2,11 @@
 # imagenet correctness tests
 MIGX=${MIGX:="../tools/migx/build/migx"}
 IMAGENET=${IMAGENET:="../datasets/imagenet"}
+TEST_RESULTDIR=${TEST_RESULTDIR:="../test-results"}
 MODELDIR=${MODELDIR:="../saved-models"}
+
+testdir=imagenet-`date '+%Y-%m-%d-%H-%M'`
+mkdir $testdir
 
 while read tag typeopt model opts
 do
@@ -10,7 +14,7 @@ do
 	continue
     fi
     echo $tag
-    ${MIGX} --imagenet ${IMAGENET} $typeopt ${MODELDIR}/$model $opts 2>${tag}.err | tee ${tag}.out
+    ${MIGX} --imagenet ${IMAGENET} $typeopt ${MODELDIR}/$model $opts 2>${testdir}/${tag}.err | tee ${testidr}/${tag}.out
 done<<EOF
 resnet50v2-onnx --onnx torchvision/resnet50i1.onnx --argname input.1
 inceptionv3-onnx --onnx torchvision/inceptioni1.onnx --argname input.1
