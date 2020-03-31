e7f21a3627e209e0570108af8a228e75787e64b0
Mike Vermeulen <mike@vermeulen.com>
2020-03-31 14:56:09 -0500

    add my specific docker and imagenet scripts.

---
+++ scripts/run_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ `id -u` != 0 ]; then
+    echo script should be run as root
+    exit 0
+fi
+
+docker run -it --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev rocm-migraphx:20200331 /bin/bash
---
+++ scripts/run_imagenet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# imagenet correctness tests
+MIGX=${MIGX:="../tools/migx/build/migx"}
+IMAGENET=${IMAGENET:="../datasets/imagenet"}
+MODELDIR=${MODELDIR:="../saved-models"}
+
+while read tag typeopt model opts
+do
+    if [ "$tag" == "#" ]; then
+	continue
+    fi
+    echo $tag
+    echo ${MIGX} --imagenet ${IMAGENET} $typeopt ${MODELDIR}/$model $opts 2>${tag}.err | tee ${tag}.out
+done<<EOF
+resnet50v2-onnx --onnx torchvision/resnet50i1.onnx --argname input.1
+inceptionv3-onnx --onnx torchvision/inceptioni1.onnx --argname input.1
+resnet50v2-tf --tfpb slim/resnet50v2_i1.pb --argname input
+mobilenet-tf --tfpb slim/mobilenet_i1.pb --argname input
+EOF
