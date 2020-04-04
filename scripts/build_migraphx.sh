21df7733fc715ce94e6d135b47372064fe7eed35
Mike Vermeulen <mike@vermeulen.com>
2020-04-04 11:56:16 -0500

    Fix up scripts for running in Docker.

--- scripts/build_migraphx.sh
+++ scripts/build_migraphx.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 #
 # build migraphx in docker container
+env DOCKER=${DOCKER:="1"}
 cd /src/AMDMIGraphX
 if [ -d build ]; then
     rm -rf build
 fi
 mkdir build
 cd build
-#env CXX=/opt/rocm/bin/hcc CXXFLAGS="-O3 -fno-gpu-rdc -amdgpu-target=gfx906" cmake ..
-env CXX=/opt/rocm/bin/hcc CXXFLAGS="-O3 -fno-gpu-rdc" cmake ..
+# workaround for hip packaging in docker
+if [ "$DOCKER" = "1" ]; then
+    env CXX=/opt/rocm/bin/hcc CXXFLAGS="-O3 -fno-gpu-rdc -amdgpu-target=gfx906" cmake ..
+else
+    env CXX=/opt/rocm/bin/hcc CXXFLAGS="-O3 -fno-gpu-rdc" cmake ..
+fi
+
 if [ "$LD_LIBRARY_PATH" = "" ]; then
     export LD_LIBRARY_PATH=/usr/local/lib:
 fi
