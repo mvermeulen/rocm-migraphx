7128485be363dbbc77c96323b024bc8b68307c67
Mike Vermeulen <mike@vermeulen.com>
2020-04-07 06:27:07 -0500

    Add HIP_DOCKER_WORKAROUND variable.

--- scripts/build_migraphx.sh
+++ scripts/build_migraphx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # build migraphx in docker container
-env DOCKER=${DOCKER:="1"}
+env HIP_DOCKER_WORKAROUND=${HIP_DOCKER_WORKAROUND:="0"}
 cd /src/AMDMIGraphX
 if [ -d build ]; then
     rm -rf build
@@ -9,7 +9,7 @@ fi
 mkdir build
 cd build
 # workaround for hip packaging in docker
-if [ "$DOCKER" = "1" ]; then
+if [ "$HIP_DOCKER_WORKAROUND" = "1" ]; then
     env CXX=/opt/rocm/bin/hcc CXXFLAGS="-O3 -fno-gpu-rdc -amdgpu-target=gfx906" cmake ..
 else
     env CXX=/opt/rocm/bin/hcc CXXFLAGS="-O3 -fno-gpu-rdc" cmake ..
