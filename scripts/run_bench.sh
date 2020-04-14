75ff64402f5286f9fb998e91aa304503a6f359f2
Mike Vermeulen <mike@vermeulen.com>
2020-04-14 14:06:36 -0500

    Change migraphx to Python2

--- scripts/run_bench.sh
+++ scripts/run_bench.sh
@@ -25,8 +25,8 @@ fi
 
 echo "mobilenet"
 if [ "$RUN_MIGX" = "1" ]; then
-    /usr/bin/python3 ${SCRIPTS}/bench.py --model mobilenet --batch 1 --framework migraphx --save_file ${SAVED_MODELS}/slim/mobilenet_i1.pb --image_file ${IMAGE} 1>mobileneti1_migx.out 2> mobileneti64_migx.err
-    /usr/bin/python3 ${SCRIPTS}/bench.py --model mobilenet --batch 64 --framework migraphx --save_file ${SAVED_MODELS}/slim/mobilenet_i64.pb --image_file ${IMAGE} 1>mobileneti64_migx.out 2> mobileneti64_migx.err
+    /usr/bin/python ${SCRIPTS}/bench.py --model mobilenet --batch 1 --framework migraphx --save_file ${SAVED_MODELS}/slim/mobilenet_i1.pb --image_file ${IMAGE} 1>mobileneti1_migx.out 2> mobileneti64_migx.err
+    /usr/bin/python ${SCRIPTS}/bench.py --model mobilenet --batch 64 --framework migraphx --save_file ${SAVED_MODELS}/slim/mobilenet_i64.pb --image_file ${IMAGE} 1>mobileneti64_migx.out 2> mobileneti64_migx.err
 fi
 
 /usr/bin/python3 ${SCRIPTS}/bench.py --model mobilenet --batch 1 --framework tensorflow --save_file ${SAVED_MODELS}/slim/mobilenet_i1.pb --image_file ${IMAGE} 1>mobileneti1_tf.out 2> mobileneti64_tf.err
@@ -34,8 +34,8 @@ fi
 
 echo "inception"
 if [ "$RUN_MIGX" = "1" ]; then
-    /usr/bin/python3 ${SCRIPTS}/bench.py --model inceptionv3 --resize_val 299 --batch 1 --framework migraphx --save_file ${SAVED_MODELS}/slim/inceptionv3_i1.pb --image_file ${IMAGE} 1>inceptionv3i1_migx.out 2> inceptionv3i1_migx.err
-    /usr/bin/python3 ${SCRIPTS}/bench.py --model inceptionv3 --resize_val 299 --batch 32 --framework migraphx --save_file ${SAVED_MODELS}/slim/inceptionv3_i32.pb --image_file ${IMAGE} 1>inceptionv3i32_migx.out 2> inceptionv3i32_migx.err
+    /usr/bin/python ${SCRIPTS}/bench.py --model inceptionv3 --resize_val 299 --batch 1 --framework migraphx --save_file ${SAVED_MODELS}/slim/inceptionv3_i1.pb --image_file ${IMAGE} 1>inceptionv3i1_migx.out 2> inceptionv3i1_migx.err
+    /usr/bin/python ${SCRIPTS}/bench.py --model inceptionv3 --resize_val 299 --batch 32 --framework migraphx --save_file ${SAVED_MODELS}/slim/inceptionv3_i32.pb --image_file ${IMAGE} 1>inceptionv3i32_migx.out 2> inceptionv3i32_migx.err
 fi
 
 /usr/bin/python3 ${SCRIPTS}/bench.py --model inceptionv3 --resize_val 299 --batch 1 --framework tensorflow --save_file ${SAVED_MODELS}/slim/inceptionv3_i1.pb --image_file ${IMAGE} 1>inceptionv3i1_tf.out 2> inceptionv3i1_tf.err
