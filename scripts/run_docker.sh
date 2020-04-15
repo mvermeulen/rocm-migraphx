fb4526497c838c7edab69c36864cf2ed87d1bad9
Mike Vermeulen <mike@vermeulen.com>
2020-04-15 10:44:38 -0500

    Update docker version.

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -4,4 +4,4 @@ if [ `id -u` != 0 ]; then
     exit 0
 fi
 
-docker run -it --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev rocm-migraphx:20200407 /bin/bash
+docker run -it --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev rocm-migraphx:20200415 /bin/bash
