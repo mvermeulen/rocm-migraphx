e88d6503b051342842aa9f10ceeac3ba13a4366c
Mike Vermeulen <mike@vermeulen.com>
2020-05-21 13:15:11 -0500

    Work around rocblas issue.

--- scripts/build_migraphx_rocm35.sh
+++ scripts/build_migraphx_rocm35.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cd /src/AMDMIGraphX
+
+# work around broken rocblas header file
+sed -i 's/rocblas-types.h/rocblas.h/g' /src/AMDMIGraphX/src/targets/gpu/gemm_impl.cpp
 git checkout master
 export LC_ALL=C.UTF-8
-rbuild -d depend --cxx=/opt/rocm/llvm/bin/clang++
+rbuild build -d depend --cxx=/opt/rocm/llvm/bin/clang++
 cd build
-make | tee -a build.log
+make -j4 | tee -a build.log
