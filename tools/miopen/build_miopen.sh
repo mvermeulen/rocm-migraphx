7c249f732cff60e2eba77826b390afb3169382ea
Mike Vermeulen <mike@vermeulen.com>
2020-05-28 11:26:02 -0500

    Add MIOpen script.

---
+++ tools/miopen/build_miopen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+RELEASE=${RELEASE:="2.3.0"}
+git clone https://github.com/MIOpen
+cd MIOpen
+git checkout $RELEASE
+cmake -P install_deps.cmake --minimum --prefix /usr/local
+mkdir build
+cd build
+env CXX=/opt/rocm/hcc/bin/hcc cmake -DMIOPEN_BACKEND=HIP -DCMAKE_PREFIX_PATH="/opt/rocm/hcc;/opt/rocm/hip" ..
+make
+
