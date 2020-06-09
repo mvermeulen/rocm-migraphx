8d0e4dab5d4fed0c904d979f8ac9e7427d878188
Mike Vermeulen <mike@vermeulen.com>
2020-06-09 10:45:48 -0500

    Fix up build_migx to use clang instead of hcc.

--- tools/migx/build_migx.sh
+++ tools/migx/build_migx.sh
@@ -6,7 +6,7 @@ if [ -d build ]; then
 fi
 mkdir build
 cd build
-if [ -f /root/hip-clang ]; then
+if [ -f /opt/rocm/llvm/bin/clang++ ]; then
     cmake -DCMAKE_CXX_COMPILER=/opt/rocm/llvm/bin/clang++ CXXFLAGS="-I/usr/local/include" ..
 else
     cmake -DCMAKE_CXX_COMPILER=/opt/rocm/bin/hcc CXXFLAGS="-I/usr/local/include" ..
