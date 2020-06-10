dfec5bd670c8d24c38696f3867c81f2812e04f01
Mike Vermeulen <mike@vermeulen.com>
2020-06-10 16:47:48 -0500

    Add support to run CPU provider as well as GPU provider.

--- scripts/run_ort_mev.sh
+++ scripts/run_ort_mev.sh
@@ -6,6 +6,7 @@
 #       tools/onnxruntime directory scripts
 TEST_RESULTDIR=${TEST_RESULTDIR:="/home/mev/source/rocm-migraphx/test-results"}
 TESTDRIVER=${TESTDRIVER:="/home/mev/source/rocm-migraphx/tools/onnxruntime/run_onnx_test.sh"}
+RUNCPU=${RUNCPU:="no"}
 
 cd ${TEST_RESULTDIR}
 testdir=ort-`date '+%Y-%m-%d-%H-%M'`
@@ -15,7 +16,7 @@ cd $testdir
 while read testcase
 do
     echo $testcase
-    env TESTCASE=$testcase $TESTDRIVER
+    env TESTCASE=$testcase RUNCPU=$RUNCPU $TESTDRIVER
 done <<TESTLIST
 opset10/BERT_Squad
 opset11/tf_resnet_v2_50
--- tools/onnxruntime/run_onnx_test.sh
+++ tools/onnxruntime/run_onnx_test.sh
@@ -11,8 +11,16 @@ TESTCASE=${TESTCASE:="opset11/tf_resnet_v2_50"}
 ITERATIONS=${ITERATIONS:="1001"}
 ONNXMODELDIR=${ONNXMODELDIR:="/home/mev/source/rocm-migraphx/saved-models/onnxruntime"}
 ONNXRUNNER=${ONNXRUNNER:="/code/onnxruntime/build/Linux/Release/onnx_test_runner"}
+RUNCPU=${RUNCPU:="no"}
 
 base=`basename $TESTCASE`
 
 /usr/bin/time -p -o $base.time1 $ONNXRUNNER -c 1 -r 1 -e migraphx $ONNXMODELDIR/$TESTCASE 1>$base.out1 2>$base.err1
 /usr/bin/time -p -o $base.time${ITERATIONS}  $ONNXRUNNER -c 1 -r ${ITERATIONS} -e migraphx $ONNXMODELDIR/$TESTCASE 1>$base.out${ITERATIONS} 2>$base.err${ITERATIONS}
+
+base_cpu=${base}_cpu
+if [ "$RUNCPU" != "no" ]; then
+    /usr/bin/time -p -o $base_cpu.time1 $ONNXRUNNER -c 1 -r 1 -e cpu $ONNXMODELDIR/$TESTCASE 1>$base_cpu.out1 2>$base_cpu.err1
+    /usr/bin/time -p -o $base_cpu.time${ITERATIONS}  $ONNXRUNNER -c 1 -r ${ITERATIONS} -e cpu $ONNXMODELDIR/$TESTCASE 1>$base_cpu.out${ITERATIONS} 2>$base_cpu.err${ITERATIONS}
+fi
+
