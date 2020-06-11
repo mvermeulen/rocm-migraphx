15f360cc5e0c00cedc508e1f405617b1f45d3467
Mike Vermeulen <mike@vermeulen.com>
2020-06-10 20:24:57 -0500

    Fix reference to mobilenet.

--- scripts/run_ort_mev.sh
+++ scripts/run_ort_mev.sh
@@ -21,7 +21,7 @@ done <<TESTLIST
 opset10/BERT_Squad
 opset11/tf_resnet_v2_50
 opset11/tf_inception_v4
-opset11/tf_mobilenet_v2_1.4._224
+opset11/tf_mobilenet_v2_1.4_224
 opset10/faster_rcnn
 opset10/mask_rcnn
 opset10/mlperf_resnet
