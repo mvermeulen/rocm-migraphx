91011bd26bed54f66e2fc9c831903b8567fd9fc0
Mike Vermeulen <mike@vermeulen.com>
2020-06-23 16:50:04 -0500

    Remove parallel build.

--- tools/onnxruntime/build_migraphx_docker.sh
+++ tools/onnxruntime/build_migraphx_docker.sh
@@ -10,7 +10,7 @@ if [ `id -u` != 0 ]; then
 fi
 cp miopen33/*.txt miopen33/*.udb miopen33/*.lock onnxruntime/dockerfiles
 cd onnxruntime/dockerfiles
-sed -e 's/debian/3.3/g' -e 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' Dockerfile.migraphx > Dockerfile.migraphx-rocm33
+sed -e 's/debian/3.3/g' -e 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' -e 's/--parallel//g' Dockerfile.migraphx > Dockerfile.migraphx-rocm33
 echo "RUN apt-get install -y time bc" >> Dockerfile.migraphx-rocm33
 echo "COPY gfx906_60.HIP.2_3_0.ufdb.txt miopen.udb miopen.udb.lock /root/.config/miopen/" >> Dockerfile.migraphx-rocm33
 docker build -f Dockerfile.migraphx-rocm33 -t rocm-migraphx-ort:$DATESTAMP-rocm33 .
