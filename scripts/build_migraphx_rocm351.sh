5c57be559b1d0efa38934e73974eb958f4fcf7de
Mike Vermeulen <mike@vermeulen.com>
2020-06-23 14:33:53 -0500

    Add building for ROCm 3.5.1.

---
+++ scripts/build_migraphx_rocm351.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+
+if [ `id -u` != 0 ]; then
+    echo script should be run as root
+    exit 0
+fi
+
+cd ../dockerfiles
+DOCKERFILE=dockerfile-rocm351-migraphx
+
+docker build -t rocm-migraphx:rocm35-migraphx -f ${DOCKERFILE} .
