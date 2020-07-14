8664f600933900de21401482aac6e5559000141a
Mike Vermeulen <mike@vermeulen.com>
2020-07-14 15:04:26 -0500

    Add cuda docker.

---
+++ tools/onnxruntime/build_cuda_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DATESTAMP=`date '+%Y%m%d'`
+if [ ! -f onnxruntime/dockerfiles/Dockerfile.cuda ]; then
+    echo "onnxruntime/dockerfiles/Dockerfile.cuda is missing"
+    exit 0
+fi
+if [ `id -u` != 0 ]; then
+    echo script should be run as root
+    exit 0
+fi
+cd onnxruntime/dockerfiles
+sed 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' Dockerfile.cuda > Dockerfile.cuda-ort
+echo "RUN apt-get install -y time bc" >> Dockerfile.cuda-ort
+
+docker build -f Dockerfile.cuda-ort -t cuda-ort:$DATESTAMP-rocm33 .
