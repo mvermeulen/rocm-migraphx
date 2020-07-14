23c655072467df6925d758f54c6c23b39552836d
Mike Vermeulen <mike@vermeulen.com>
2020-07-14 16:18:12 -0500

    Modify openvino docker.

--- tools/onnxruntime/build_openvino_docker.sh
+++ tools/onnxruntime/build_openvino_docker.sh
@@ -9,7 +9,7 @@ if [ `id -u` != 0 ]; then
     exit 0
 fi
 cd onnxruntime/dockerfiles
-sed 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' Dockerfile.openvino > Dockerfile.openvino-ort
+sed 's?/code/onnxruntime /code/cmake-3.14.3-Linux-x86_64?/code/cmake-3.14.3-Linux-x86_64?g' Dockerfile.openvino > Dockerfile.openvino-ort
 echo "RUN apt-get install -y time bc" >> Dockerfile.openvino-ort
 
 docker build -f Dockerfile.openvino-ort -t openvino-ort:$DATESTAMP .
