60ca36f928a8860580fe8e21ad6e5965ef49384b
Mike Vermeulen <mike@vermeulen.com>
2020-07-14 16:24:25 -0500

    Add tensorrt script.

---
+++ tools/onnxruntime/build_tensorrt_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DATESTAMP=`date '+%Y%m%d'`
+if [ ! -f onnxruntime/dockerfiles/Dockerfile.tensorrt ]; then
+    echo "onnxruntime/dockerfiles/Dockerfile.tensorrt is missing"
+    exit 0
+fi
+if [ `id -u` != 0 ]; then
+    echo script should be run as root
+    exit 0
+fi
+cd onnxruntime/dockerfiles
+sed 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' Dockerfile.tensorrt > Dockerfile.tensorrt-ort
+echo "RUN apt-get install -y time bc" >> Dockerfile.tensorrt-ort
+
+docker build -f Dockerfile.tensorrt-ort -t tensorrt-ort:$DATESTAMP-rocm33 .
