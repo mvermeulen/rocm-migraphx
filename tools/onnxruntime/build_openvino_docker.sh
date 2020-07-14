672bb97a8b734af8225de47a7567be8cfe08f63b
Mike Vermeulen <mike@vermeulen.com>
2020-07-14 16:16:51 -0500

    Add OpenVino docker.

--- tools/onnxruntime/build_openvino_docker.sh
+++ tools/onnxruntime/build_openvino_docker.sh
@@ -9,7 +9,7 @@ if [ `id -u` != 0 ]; then
     exit 0
 fi
 cd onnxruntime/dockerfiles
-sed 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' Dockerfile.source > Dockerfile.source-ort
-echo "RUN apt-get install -y time bc" >> Dockerfile.source-ort
+sed 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' Dockerfile.openvino > Dockerfile.openvino-ort
+echo "RUN apt-get install -y time bc" >> Dockerfile.openvino-ort
 
-docker build -f Dockerfile.source-ort -t cpu-ort:$DATESTAMP .
+docker build -f Dockerfile.openvino-ort -t openvino-ort:$DATESTAMP .
