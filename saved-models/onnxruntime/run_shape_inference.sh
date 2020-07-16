29ac06a3b6b01438d4a84b17bd5cfab93268fb2b
Mike Vermeulen <mike@vermeulen.com>
2020-07-16 10:56:26 -0500

    Update script to run shape inference to also copy test directories.

--- saved-models/onnxruntime/run_shape_inference.sh
+++ saved-models/onnxruntime/run_shape_inference.sh
@@ -15,3 +15,12 @@ opset10/faster_rcnn/faster_rcnn_R_50_FPN_1x.onnx inferred/faster_rcnn/faster_rcn
 opset10/mask_rcnn/mask_rcnn_R_50_FPN_1x.onnx inferred/mask_rcnn/mask_rcnn_R_50_FPN_1x_inf.onnx
 INFERLIST
 
+while read input output
+do
+    echo cp -r $input $output
+    cp -r $input $output
+    done <<COPYLIST
+opset10/BERT_Squad/test_data_set_1 inferred/BERT_Squad/
+opset10/faster_rcnn/test_data_set_0 inferred/faster_rcnn/
+opset10/mask_rcnn/test_data_set_0 inferred/mask_rcnn/
+COPYLIST
