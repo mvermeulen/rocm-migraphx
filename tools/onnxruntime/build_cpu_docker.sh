1e5db3789a340c3902cd5f2b041cfcc491e84a94
Mike Vermeulen <mike@vermeulen.com>
2020-07-16 14:20:47 -0500

    Add calc-median directly.

--- tools/onnxruntime/build_cpu_docker.sh
+++ tools/onnxruntime/build_cpu_docker.sh
@@ -10,6 +10,7 @@ if [ `id -u` != 0 ]; then
 fi
 cd onnxruntime/dockerfiles
 sed 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' Dockerfile.source > Dockerfile.source-ort
-echo "RUN apt-get install -y time bc calc-stats" >> Dockerfile.source-ort
+echo "RUN apt-get install -y time bc" >> Dockerfile.source-ort
+echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.source-ort
 echo "ENV EXPROVIDER=cpu" >> Dockerfile.source-ort
 docker build --no-cache -f Dockerfile.source-ort -t ort:cpu-$DATESTAMP .
