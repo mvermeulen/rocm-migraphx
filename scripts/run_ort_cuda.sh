d5bcae05c7d3c4b3d6f44e526fced3038a0e35f7
Mike Vermeulen <mike@vermeulen.com>
2020-07-27 19:19:28 -0500

    Change to this week.

--- scripts/run_ort_cuda.sh
+++ scripts/run_ort_cuda.sh
@@ -1,1 +1,1 @@
-docker run --gpus all --network=host -v /home/mev:/home/mev mevermeulen/ort:cuda-20200714 env EXPROVIDER=cuda /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
+docker run --gpus all --network=host -v /home/mev:/home/mev mevermeulen/ort:cuda-20200727 env EXPROVIDER=cuda /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
--- scripts/run_ort_tensorrt.sh
+++ scripts/run_ort_tensorrt.sh
@@ -1,1 +1,1 @@
-docker run --gpus all --network=host -v /home/mev:/home/mev mevermeulen/ort:tensorrt-20200714 env EXPROVIDER=tensorrt /home/mev/source/rocm-migraphx/scripts/run_ort_infer.sh
+docker run --gpus all --network=host -v /home/mev:/home/mev mevermeulen/ort:tensorrt-20200727 env EXPROVIDER=tensorrt /home/mev/source/rocm-migraphx/scripts/run_ort_infer.sh
