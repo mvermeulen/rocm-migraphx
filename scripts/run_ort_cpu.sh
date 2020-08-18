010decaf4cdcd6341fd37235c4c6499226ad4d1c
Mike Vermeulen <mike@vermeulen.com>
2020-08-18 11:56:38 -0500

    Update ORT instances.

--- scripts/run_ort_cpu.sh
+++ scripts/run_ort_cpu.sh
@@ -1,1 +1,1 @@
-docker run --network=host -v /home/mev:/home/mev mevermeulen/ort:cpu-20200811 env EXPROVIDER=cpu /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
+docker run --network=host -v /home/mev:/home/mev mevermeulen/ort:cpu-20200816 env EXPROVIDER=cpu /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
