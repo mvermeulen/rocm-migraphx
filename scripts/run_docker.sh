18a69b2e90f22ac97b696d085ad75fe1ca0e9723
Mike Vermeulen <mike@vermeulen.com>
2020-08-21 13:08:39 -0500

    Update to 3.7.

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -4,4 +4,4 @@ if [ `id -u` != 0 ]; then
     exit 0
 fi
 
-docker run -it --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev rocm-migraphx:20200816 /bin/bash
+docker run -it --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev rocm-migraphx:3.7 /bin/bash
