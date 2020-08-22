2fe2253ae0aa0909dc7a28a1f30dad4e5585acdc
Mike Vermeulen <mike@vermeulen.com>
2020-08-21 19:14:01 -0500

    Echo what is being tuned.

--- tools/miopen/perf-rocm37/tune_unique.sh
+++ tools/miopen/perf-rocm37/tune_unique.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+export MIOPEN_FIND_ENFORCE=3
 for f in *unique.sh
 do
     base=`basename $f .sh`
     bash $f > ${base}.out 2> ${base}.err
+    echo ------
+    date
+    echo $f
     rm $f
 done
