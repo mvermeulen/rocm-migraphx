c756476737927efe836d1a2e2f74ef902f509ae0
Mike Vermeulen <mike@vermeulen.com>
2020-08-21 19:17:26 -0500

    Update unique script and save miopen tunings.

--- tools/miopen/perf-rocm37/tune_unique.sh
+++ tools/miopen/perf-rocm37/tune_unique.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 export MIOPEN_FIND_ENFORCE=3
+MIOPENDIR=/root/.config/miopen
+count=0
 for f in *unique.sh
 do
+    count=$((count+1))
     base=`basename $f .sh`
     bash $f > ${base}.out 2> ${base}.err
+    cp -r ${MIOPENDIR} /home/mev/miopen-${count}
     echo ------
     date
     echo $f
-    rm $f
+    mv $f /home/mev//miopen-${count}
 done
