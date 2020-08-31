35a242d3a57bcf453d4a319071a1e08f891dc3b1
Mike Vermeulen <mike@vermeulen.com>
2020-08-31 16:26:19 -0500

    Update weekly docker.

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -4,4 +4,4 @@ if [ `id -u` != 0 ]; then
     exit 0
 fi
 
-docker run -it --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev rocm-migraphx:20200825 /bin/bash
+docker run -it --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev rocm-migraphx:20200831 /bin/bash
