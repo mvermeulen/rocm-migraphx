3104b11e021db977b2030df6263d85a7cf24710c
Mike Vermeulen <mike@vermeulen.com>
2020-09-22 12:04:45 -0500

    Rename test directory.

--- scripts/run_ort_openvino.sh
+++ scripts/run_ort_openvino.sh
@@ -1,1 +1,1 @@
-docker run --network=host -v /home/mev:/home/mev mevermeulen/ort:openvino-20200921 env EXPROVIDER=cpu /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
+docker run --network=host -v /home/mev:/home/mev mevermeulen/ort:openvino-20200921 env EXPROVIDER=openvino /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
