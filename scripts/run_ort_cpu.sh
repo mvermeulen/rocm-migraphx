1f0f289ce5bf41225b0d6ff33cc553ad4e4e11f4
Mike Vermeulen <mike@vermeulen.com>
2020-09-28 21:19:03 -0500

    Make sure we can mount /extra.

--- scripts/run_ort_cpu.sh
+++ scripts/run_ort_cpu.sh
@@ -1,1 +1,8 @@
-docker run --network=host -v /home/mev:/home/mev mevermeulen/ort:cpu-20200928 env EXPROVIDER=cpu /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
+#!/bin/bash
+if [ -d /extra ]; then
+    EXTRAMOUNT="-v /extra:extra"
+else
+    EXTRAMOUNT=""
+fi
+
+docker run --network=host -v /home/mev:/home/mev $EXTRAMOUNT mevermeulen/ort:cpu-20200928 env EXPROVIDER=cpu /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
