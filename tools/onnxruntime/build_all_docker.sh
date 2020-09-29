46ef24e4505335ecee887e1bbfb3b24a01bf13e1
Mike Vermeulen <mike@vermeulen.com>
2020-09-28 19:08:24 -0500

    Update to 9/28.

--- tools/onnxruntime/build_all_docker.sh
+++ tools/onnxruntime/build_all_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./build_cpu_docker.sh 2>&1 | tee build_cpu.txt
-./build_migraphx_docker.sh 2>&1 | tee build_migraphx.txt
 ./build_cuda_docker.sh 2>&1 | tee build_cuda.txt
 ./build_tensorrt_docker.sh 2>&1 | tee build_tensorrt.txt
+./build_openvino_docker.sh 2>&1 | tee build_openvino.txt
+./build_migraphx_docker.sh 2>&1 | tee build_migraphx.txt
