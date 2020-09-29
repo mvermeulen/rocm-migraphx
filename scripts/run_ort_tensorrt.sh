5eed4e84c38cca60c63bc90125039d67a0bf6dcb
Mike Vermeulen <mike@vermeulen.com>
2020-09-28 19:12:37 -0500

    Update to 9/28.

--- scripts/run_ort_tensorrt.sh
+++ scripts/run_ort_tensorrt.sh
@@ -1,1 +1,1 @@
-docker run --gpus all --network=host -v /home/mev:/home/mev mevermeulen/ort:tensorrt-20200921 env EXPROVIDER=tensorrt /home/mev/source/rocm-migraphx/scripts/run_ort_infer.sh
+docker run --gpus all --network=host -v /home/mev:/home/mev mevermeulen/ort:tensorrt-20200928 env EXPROVIDER=tensorrt /home/mev/source/rocm-migraphx/scripts/run_ort_infer.sh
