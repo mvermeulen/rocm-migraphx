5e6a196df9af124e90afa744968bdef8f797b8f1
Mike Vermeulen <mike@vermeulen.com>
2020-10-12 13:35:46 -0500

    Update to add --no-cache.

--- tools/onnxruntime/build_openvino_docker.sh
+++ tools/onnxruntime/build_openvino_docker.sh
@@ -14,4 +14,4 @@ echo "RUN apt-get install -y time bc" >> Dockerfile.openvino-ort
 echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.openvino-ort
 echo "ENV EXPROVIDER=openvino" >> Dockerfile.openvino-ort
 
-docker build -f Dockerfile.openvino-ort -t ort:openvino-$DATESTAMP .
+docker build --no-cache -f Dockerfile.openvino-ort -t ort:openvino-$DATESTAMP .
