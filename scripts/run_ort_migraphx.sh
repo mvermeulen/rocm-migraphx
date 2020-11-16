df2207028edefcaf37844869a947dd8e9c7313bb
Mike Vermeulen <mike@vermeulen.com>
2020-11-16 15:46:22 -0600

    Update the date.

--- scripts/run_ort_migraphx.sh
+++ scripts/run_ort_migraphx.sh
@@ -1,1 +1,1 @@
-docker run --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev ort:migraphx-rocm37-20201109 env EXPROVIDER=migraphx /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
+docker run --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev ort:migraphx-rocm37-20201116 env EXPROVIDER=migraphx /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
