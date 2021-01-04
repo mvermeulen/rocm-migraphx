c5df697548ef8d6dd628e8356330b46c474e9f32
Mike Vermeulen <mike@vermeulen.com>
2021-01-04 09:19:08 -0600

    Update dates.

--- scripts/run_ort_openvino.sh
+++ scripts/run_ort_openvino.sh
@@ -5,4 +5,4 @@ else
     EXTRAMOUNT=""
 fi
 
-docker run -e TZ=America/Chicago --network=host -v /home/mev:/home/mev $EXTRAMOUNT ort:openvino-20201214 env EXPROVIDER=openvino /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
+docker run -e TZ=America/Chicago --network=host -v /home/mev:/home/mev $EXTRAMOUNT ort:openvino-20210104 env EXPROVIDER=openvino /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
