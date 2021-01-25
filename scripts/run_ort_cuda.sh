2a63be898b4bfab59d14ce6c7d3214c38dd0484b
Mike Vermeulen <mike@vermeulen.com>
2021-01-25 12:44:14 -0600

    Update dates.

--- scripts/run_ort_cuda.sh
+++ scripts/run_ort_cuda.sh
@@ -1,1 +1,1 @@
-docker run -e TZ=America/Chicago --gpus all --network=host -v /home/mev:/home/mev ort:cuda-20210118 env EXPROVIDER=cuda /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
+docker run -e TZ=America/Chicago --gpus all --network=host -v /home/mev:/home/mev ort:cuda-20210125 env EXPROVIDER=cuda /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
