1f76f74056891ecc0712bee2c503f632c002d247
Mike Vermeulen <mike@vermeulen.com>
2021-02-28 16:59:18 -0600

    Fix up LD_LIBRARY_PATH issue.

--- tools/onnxruntime/build_openvino_docker.sh
+++ tools/onnxruntime/build_openvino_docker.sh
@@ -13,7 +13,7 @@ sed 's?&& rm -rf onnxruntime??g' Dockerfile.openvino > Dockerfile.openvino-ort
 echo "RUN apt-get install -y time bc" >> Dockerfile.openvino-ort
 echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.openvino-ort
 echo "ENV EXPROVIDER=openvino" >> Dockerfile.openvino-ort
-echo "RUN apt-get install -y linux-tools-5.4.0-54-generic linux-cloud-tools-5.4.0-54-generic linux-tools-generic linux-cloud-tools-generic" >> Dockerfile.openvino-ort
+echo "RUN env LD_LIBRARY_PATH= apt-get install -y linux-tools-5.4.0-54-generic linux-cloud-tools-5.4.0-54-generic linux-tools-generic linux-cloud-tools-generic" >> Dockerfile.openvino-ort
 cd ..
 
 docker build --no-cache -f dockerfiles/Dockerfile.openvino-ort -t ort:openvino-$DATESTAMP .
