9a7112cc02b6abdbb4c723aa26b546a3cdf13d2c
Mike Vermeulen <mike@vermeulen.com>
2021-03-29 20:25:42 -0500

    Update dates.

--- scripts/run_ort_cpu.sh
+++ scripts/run_ort_cpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER=${DOCKER:="ort:cpu-20210322"}
+DOCKER=${DOCKER:="ort:cpu-20210329"}
 
 if [ -d /extra ]; then
     EXTRAMOUNT="-v /extra:/extra"
--- scripts/run_ort_openvino.sh
+++ scripts/run_ort_openvino.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER=${DOCKER:="ort:openvino-20210215"}
+DOCKER=${DOCKER:="ort:openvino-20210329"}
 
 if [ -d /extra ]; then
     EXTRAMOUNT="-v /extra:/extra"
