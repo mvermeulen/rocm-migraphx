9108ca6f64713aba88122fdd9b333698810898b1
Mike Vermeulen <mike@vermeulen.com>
2021-05-07 16:54:07 -0500

    Fix protobuf.

--- tools/onnxruntime/build_migraphx_docker.sh
+++ tools/onnxruntime/build_migraphx_docker.sh
@@ -11,7 +11,7 @@ if [ `id -u` != 0 ]; then
 fi
 cp calc-median miopen37/*.txt miopen37/*.udb miopen37/*.lock onnxruntime/dockerfiles
 cd onnxruntime/dockerfiles
-sed -e 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' -e 's/--parallel//g' -e 's/debian/3.7/g' -e 's/migraphx_for_ort/develop/g' Dockerfile.migraphx > Dockerfile.migraphx-rocm37
+sed -e 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' -e 's/--parallel//g' -e 's/debian/3.7/g' -e 's/migraphx_for_ort/upgrade_protobuf_to_3.16/g' Dockerfile.migraphx > Dockerfile.migraphx-rocm37
 echo "RUN apt-get install -y time bc" >> Dockerfile.migraphx-rocm37
 echo "COPY gfx906_60.HIP.2_6_0_8145-rocm-rel-3.7-20-c16087a4.ufdb.txt miopen_1.0.0.udb miopen.udb.lock /root/.config/miopen/" >> Dockerfile.migraphx-rocm37
 echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.migraphx-rocm37
