378a4131c3e93e563bb496c4f0aa1070d5410836
Mike Vermeulen <mike@vermeulen.com>
2021-06-14 17:43:46 -0500

    Fix up MIGraphX.

--- scripts/run_ort_migraphx.sh
+++ scripts/run_ort_migraphx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-DOCKER=${DOCKER:="ort:migraphx-rocm37-20210518"}
+DOCKER=${DOCKER:="ort:migraphx-rocm42-20210614"}
 
 docker run -e TZ=America/Chicago --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev ${DOCKER} env EXPROVIDER=migraphx /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
