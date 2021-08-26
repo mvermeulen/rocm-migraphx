481b48df959dc539d921dea0ef1b0e9da3a98a25
Mike Vermeulen <mike@vermeulen.com>
2021-08-26 17:23:03 -0500

    Add build_navi flag.

--- scripts/build_migraphx_docker.sh
+++ scripts/build_migraphx_docker.sh
@@ -5,8 +5,10 @@
 set -x
 #
 # Parameters
-ROCM_RELEASE=${ROCM_RELEASE:="4.2"}
+ROCM_RELEASE=${ROCM_RELEASE:="4.3"}
 ROCM_BASE=${ROCM_BASE:="rocm/dev-ubuntu-18.04:${ROCM_RELEASE}"}
+BUILD_NAVI=${BUILD_NAVI:="0"}
+
 
 if [ `id -u` != 0 ]; then
     echo script should be run as root
@@ -19,7 +21,8 @@ DOCKERFILE=dockerfile.`date '+%Y-%m-%d'`
 # use sed to create updated dockerfile.  Some might work with ARGs but
 # had difficulties with substitutions
 sed -e "s?ROCM_RELEASE?${ROCM_RELEASE}?g" \
+    -e "s?BUILD_NAVI_CHOICE?${BUILD_NAVI}?g" \
     -e "s?ROCM_BASE?${ROCM_BASE}?g" dockerfile > $DOCKERFILE
 
-docker build --no-cache -t rocm-migraphx:${ROCM_RELEASE} -f $DOCKERFILE \
+docker build --no-cache -t rocm-migraphx:${ROCM_RELEASE} -f $DOCKERFILE 
        .
