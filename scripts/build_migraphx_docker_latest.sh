cf69fdc4195767b2c8dd67d8b246d58742ad1d8d
Mike Vermeulen <mike@vermeulen.com>
2021-08-30 08:55:47 -0500

    Update builds to 4.3.

--- scripts/build_migraphx_docker_latest.sh
+++ scripts/build_migraphx_docker_latest.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
 DATESTAMP=`date '+%Y%m%d'`
-BASE=rocm-migraphx:4.2
+BASE=${BASE:="rocm-migraphx:4.3"}
+MIOPENTUNE=${MIOPENTUNE:="miopen-rocm43"}
 if [ `id -u` != 0 ]; then
     echo script should be run as root
     exit 0
 fi
 cd ../dockerfiles
-docker build -t rocm-migraphx:${DATESTAMP} --no-cache --build-arg DOCKERBASE=${BASE} -f ../dockerfiles/dockerfile-daily .
+docker build -t rocm-migraphx:${DATESTAMP} --no-cache --build-arg DOCKERBASE=${BASE} --build-arg MIOPENTUNE=${MIOPENTUNE} -f ../dockerfiles/dockerfile-daily .
