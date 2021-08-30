63e11e66d15c8362ffec15285b9292b0d305c670
Mike Vermeulen <mike@vermeulen.com>
2021-08-30 09:11:47 -0500

    Try updating docker image.

--- tools/onnxruntime/build_migraphx_docker.sh
+++ tools/onnxruntime/build_migraphx_docker.sh
@@ -11,10 +11,10 @@ if [ `id -u` != 0 ]; then
 fi
 cp calc-median miopen37/*.txt miopen37/*.udb miopen37/*.lock onnxruntime/dockerfiles
 cd onnxruntime/dockerfiles
-sed -e 's/onnxruntime cmake-3.20.3-Linux-x86_64/cmake-3.20.3-Linux-x86_64/g' -e 's/--parallel//g' -e 's/debian/4.2/g' -e 's/4.0/4.2/g' -e 's/migraphx_for_ort/develop/g' Dockerfile.migraphx > Dockerfile.migraphx-rocm42
-echo "RUN apt-get install -y time bc" >> Dockerfile.migraphx-rocm42
-echo "COPY gfx906_60.HIP.2_6_0_8145-rocm-rel-3.7-20-c16087a4.ufdb.txt miopen_1.0.0.udb miopen.udb.lock /root/.config/miopen/" >> Dockerfile.migraphx-rocm42
-echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.migraphx-rocm42
-echo "ENV EXPROVIDER=migraphx" >> Dockerfile.migraphx-rocm42
+sed -e 's/onnxruntime cmake-3.21.0-linux-x86_64/cmake-3.21.0-linux-x86_64/g' -e 's/--parallel//g' -e 's/4.2/4.3/g' -e 's/xenial/ubuntu/g' -e 's/migraphx_for_ort/develop/g' Dockerfile.migraphx > Dockerfile.migraphx-rocm43
+echo "RUN apt-get install -y time bc" >> Dockerfile.migraphx-rocm43
+echo "COPY gfx906_60.HIP.2_6_0_8145-rocm-rel-3.7-20-c16087a4.ufdb.txt miopen_1.0.0.udb miopen.udb.lock /root/.config/miopen/" >> Dockerfile.migraphx-rocm43
+echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.migraphx-rocm43
+echo "ENV EXPROVIDER=migraphx" >> Dockerfile.migraphx-rocm43
 
-docker build ${CACHE} -f Dockerfile.migraphx-rocm42 -t ort:migraphx-rocm42-$DATESTAMP .
+docker build ${CACHE} -f Dockerfile.migraphx-rocm43 -t ort:migraphx-rocm43-$DATESTAMP .
