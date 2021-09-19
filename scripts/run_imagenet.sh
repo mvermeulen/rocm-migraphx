e13cf76b00a49f2cd6b9c24ed95cd46405c41813
Mike Vermeulen <mike@vermeulen.com>
2021-09-18 19:46:43 -0500

    Add some test cases for imagenet.

--- scripts/run_imagenet.sh
+++ scripts/run_imagenet.sh
@@ -26,4 +26,12 @@ resnet50v2-onnx --onnx torchvision/resnet50i1.onnx --argname input.1
 inceptionv3-onnx --onnx torchvision/inceptioni1.onnx --argname x.1
 resnet50v2-tf --tfpb slim/resnet50v2_i1.pb --argname input
 mobilenet-tf --tfpb slim/mobilenet_i1.pb --argname input
+alexnet-onnx --onnx torchvision/alexneti1.onnx --argname input.1
+densenet-onnx --onnx torchvision/densenet121i1.onnx --argname input.1
+squeezenet-onnx --onnx torchvision/squeezenet11i1.onnx --argname input.1
+vgg16-onnx --onnx torchvision/vgg16i1.onnx --argname input.1
+dpn92-onnx --onnx cadene/dpn92i1.onnx --argname 0
+resnext101-onnx --onnx cadene/resnext101_64x4di1.onnx --argname 0
+inceptionv4-tf --tfpb slim/inceptionv4_i1.pb --argname input
+vgg16-tf --tfpb slim/vgg16_i1.pb --argname input
 EOF
