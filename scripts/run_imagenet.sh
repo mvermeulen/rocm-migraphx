7637de29d2ca990f6d973216f7f0b88ad49e315d
Mike Vermeulen <mike@vermeulen.com>
2021-09-20 12:49:38 -0500

    Comment out extra tests.

--- scripts/run_imagenet.sh
+++ scripts/run_imagenet.sh
@@ -10,7 +10,6 @@ if [ "$BUILD_NAVI" = "1" ]; then
     export MIGRAPHX_DISABLE_MIOPEN_FUSION=1
 fi
 
-
 testdir=${TEST_RESULTDIR}/imagenet-`date '+%Y-%m-%d-%H-%M'`
 mkdir $testdir
 
@@ -26,11 +25,12 @@ resnet50v2-onnx --onnx torchvision/resnet50i1.onnx --argname input.1
 inceptionv3-onnx --onnx torchvision/inceptioni1.onnx --argname x.1
 resnet50v2-tf --tfpb slim/resnet50v2_i1.pb --argname input
 mobilenet-tf --tfpb slim/mobilenet_i1.pb --argname input
-alexnet-onnx --onnx torchvision/alexneti1.onnx --argname input.1
-densenet-onnx --onnx torchvision/densenet121i1.onnx --argname input.1
-vgg16-onnx --onnx torchvision/vgg16i1.onnx --argname input.1
-dpn92-onnx --onnx cadene/dpn92i1.onnx --argname 0
-resnext101-onnx --onnx cadene/resnext101_64x4di1.onnx --argname 0
-inceptionv4-tf --tfpb slim/inceptionv4_i1.pb --argname input
-vgg16-tf --tfpb slim/vgg16_i1.pb --argname input
 EOF
+
+# Extra models that also have good accuracy behavior
+# alexnet-onnx --onnx torchvision/alexneti1.onnx --argname input.1
+# densenet-onnx --onnx torchvision/densenet121i1.onnx --argname input.1
+# vgg16-onnx --onnx torchvision/vgg16i1.onnx --argname input.1
+# dpn92-onnx --onnx cadene/dpn92i1.onnx --argname 0
+# resnext101-onnx --onnx cadene/resnext101_64x4di1.onnx --argname 0
+# inceptionv4-tf --tfpb slim/inceptionv4_i1.pb --argname input
