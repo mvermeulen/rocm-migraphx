3980a4086f04d4d7e36a4c490ee15d6222242d20
Mike Vermeulen <mike@vermeulen.com>
2021-09-21 17:42:05 -0500

    Add dump for regnet models.

--- saved-models/timm/get_pretrained.py
+++ saved-models/timm/get_pretrained.py
@@ -1,18 +1,28 @@
 import torch
 import timm
+import sys
 from pprint import pprint
 
-model_names = timm.list_models(pretrained=True)
-
-for model_name in model_names:
-    batch=1
-    model = timm.create_model(model_name,pretrained=True)
-    # try only 224x224 images for now
-    dummy=torch.randn(batch,3,224,224)    
+def onnx_224(batch,name):
+    model = timm.create_model(name,pretrained=True)
+    dummy = torch.randn(batch,3,224,224)
     model.eval()
-
     try:
-        torch.onnx.export(model,dummy,model_name+'i'+str(batch)+'.onnx')
-        print(model_name,'exported')
+        torch.onnx.export(model,dummy,name+'i'+str(batch)+'.onnx')
+        print(name,'exported')
     except Exception:
-        print(model_name,'not exported')
+        print(name,'not exported')
+
+model_names = timm.list_models(pretrained=True)
+for model_name in model_names:
+    print(model_name)
+
+regnets = ["regnetx_002","regnetx_004","regnetx_006","regnetx_008","regnetx_016","regnetx_032","regnetx_040","regnetx_064","regnetx_120","regnetx_160","regnetx_320","regnety_002","regnety_004","regnety_006","regnety_008","regnety_016","regnety_032","regnety_040","regnety_064","regnety_120","regnety_160","regnety_320"]
+for regnet in regnets:
+    onnx_224(1,regnet)
+
+sys.exit()
+
+# Try dumping every model
+for model_name in model_names:
+    onnx_224(model)
