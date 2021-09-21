f738fd642e2a5b02c9baf41f8e136406fff41842
Mike Vermeulen <mike@vermeulen.com>
2021-09-20 20:07:36 -0500

    Fix up build for CUDA.

--- tools/onnxruntime/build_cuda_docker.sh
+++ tools/onnxruntime/build_cuda_docker.sh
@@ -12,7 +12,7 @@ fi
 cp calc-median onnxruntime
 cd onnxruntime/dockerfiles
 #cp Dockerfile.cuda Dockerfile.cuda-ort
-sed -e 's/--parallel//g' Dockerfile.cuda -e 's/runtime/devel/g' > Dockerfile.cuda-ort
+sed -e 's/--parallel//g' Dockerfile.cuda > Dockerfile.cuda-ort
 echo "" >> Dockerfile.cuda-ort
 echo "RUN apt update && apt-get install -y time bc" >> Dockerfile.cuda-ort
 echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.cuda-ort
