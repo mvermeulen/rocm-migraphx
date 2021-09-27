d705cffae5e03a1234093dbfaab348833ea2bb7b
Mike Vermeulen <mike@vermeulen.com>
2021-09-27 08:45:25 -0500

    Add rocm execution provider.

---
+++ scripts/run_ort_rocm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DOCKER=${DOCKER:="ort:rocm-20210927"}
+
+docker run -e TZ=America/Chicago --gpus all --network=host -v /home/mev:/home/mev ${DOCKER} env EXPROVIDER=rocm /home/mev/source/rocm-migraphx/scripts/run_ort_mev.sh
