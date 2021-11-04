3a851500ab75e9b57d31b40652747a0067418f13
Mike Vermeulen <mike@vermeulen.com>
2021-11-04 10:03:36 -0500

    Fix up to use diff models.

--- tools/onnxruntime/build_migraphx_docker.sh
+++ tools/onnxruntime/build_migraphx_docker.sh
@@ -11,10 +11,10 @@ if [ `id -u` != 0 ]; then
 fi
 cp calc-median miopen37/*.txt miopen37/*.udb miopen37/*.lock onnxruntime/dockerfiles
 cd onnxruntime/dockerfiles
-sed -e 's/onnxruntime cmake-3.21.0-linux-x86_64/cmake-3.21.0-linux-x86_64/g' -e 's/--parallel//g' -e 's/4.2/4.3/g' -e 's/xenial/ubuntu/g' -e 's/migraphx_for_ort/develop/g' Dockerfile.migraphx > Dockerfile.migraphx-rocm43
-echo "RUN apt-get install -y time bc" >> Dockerfile.migraphx-rocm43
-echo "COPY gfx906_60.HIP.2_6_0_8145-rocm-rel-3.7-20-c16087a4.ufdb.txt miopen_1.0.0.udb miopen.udb.lock /root/.config/miopen/" >> Dockerfile.migraphx-rocm43
-echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.migraphx-rocm43
-echo "ENV EXPROVIDER=migraphx" >> Dockerfile.migraphx-rocm43
+cp Dockerfile.migraphx Dockerfile.migraphx-rocm45
+echo "RUN apt-get install -y time bc" >> Dockerfile.migraphx-rocm45
+echo "COPY gfx906_60.HIP.2_6_0_8145-rocm-rel-3.7-20-c16087a4.ufdb.txt miopen_1.0.0.udb miopen.udb.lock /root/.config/miopen/" >> Dockerfile.migraphx-rocm45
+echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.migraphx-rocm45
+echo "ENV EXPROVIDER=migraphx" >> Dockerfile.migraphx-rocm45
 
-docker build ${CACHE} --build-arg ONNXRUNTIME_REPO=https://github.com/ROCmSoftwarePlatform/onnxruntime --build-arg ONNXRUNTIME_BRANCH=amdmigraphx_fix_build_error -f Dockerfile.migraphx-rocm43 -t ort:migraphx-rocm43-$DATESTAMP .
+docker build ${CACHE} -f Dockerfile.migraphx-rocm45 -t ort:migraphx-rocm45-$DATESTAMP .
