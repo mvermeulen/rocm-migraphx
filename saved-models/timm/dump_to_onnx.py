fc910d214cdf132375849c7fe9971ffcbc628d0f
Mike Vermeulen <mike@vermeulen.com>
2021-11-05 16:10:52 -0500

    Add script to list models available and dump them.

---
+++ saved-models/timm/dump_to_onnx.py
@@ -0,0 +1,30 @@
+import torch
+import argparse
+import timm
+import sys
+from pprint import pprint
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--batch",type=int,default=1)
+parser.add_argument("--size",type=int,default=224)
+parser.add_argument("--name",type=str)
+parser.add_argument("--list",action='store_true')
+args=parser.parse_args()
+
+def dump_onnx_file(name,batch,size):
+    model = timm.create_model(name,pretrained=True)
+    dummy = torch.randn(batch,3,size,size)
+    model.eval()
+    try:
+        torch.onnx.export(model,dummy,name+'i'+str(batch)+'.onnx')
+        print(name,'exported')
+    except Exception:
+        print(name,'not exported')
+
+if args.list:
+    model_names = timm.list_models(pretrained=True)
+    for model_name in model_names:
+        print(model_name)
+else:
+    dump_onnx_file(args.name,args.batch,args.size)
+
--- saved-models/timm/get_pretrained.py
+++ saved-models/timm/get_pretrained.py
@@ -17,11 +17,13 @@ model_names = timm.list_models(pretrained=True)
 for model_name in model_names:
     print(model_name)
 
+onnx_224(1,"cspresnext50")
+
+sys.exit()
+
 regnets = ["regnetx_002","regnetx_004","regnetx_006","regnetx_008","regnetx_016","regnetx_032","regnetx_040","regnetx_064","regnetx_120","regnetx_160","regnetx_320","regnety_002","regnety_004","regnety_006","regnety_008","regnety_016","regnety_032","regnety_040","regnety_064","regnety_120","regnety_160","regnety_320"]
 for regnet in regnets:
     onnx_224(1,regnet)
-
-sys.exit()
 
 # Try dumping every model
 for model_name in model_names:
