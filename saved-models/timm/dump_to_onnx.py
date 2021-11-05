e094fb0e9a51c19de8e983073aa8be97b7d67a3e
Mike Vermeulen <mike@vermeulen.com>
2021-11-05 17:38:23 -0500

    Add option to print input size.

--- saved-models/timm/dump_to_onnx.py
+++ saved-models/timm/dump_to_onnx.py
@@ -9,6 +9,7 @@ parser.add_argument("--batch",type=int,default=1)
 parser.add_argument("--size",type=int,default=224)
 parser.add_argument("--name",type=str)
 parser.add_argument("--list",action='store_true')
+parser.add_argument("--input",action='store_true')
 args=parser.parse_args()
 
 def dump_onnx_file(name,batch,size):
@@ -25,6 +26,9 @@ if args.list:
     model_names = timm.list_models(pretrained=True)
     for model_name in model_names:
         print(model_name)
+elif args.input:
+    model = timm.create_model(args.name,pretrained=True)
+    print(model.default_cfg['input_size'])
 else:
     dump_onnx_file(args.name,args.batch,args.size)
 
