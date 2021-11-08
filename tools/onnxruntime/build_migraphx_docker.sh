865aea1956f7ef789f0f6d60388fb3ace45c33d6
Mike Vermeulen <mike@vermeulen.com>
2021-11-08 09:08:33 -0600

    Update to ROCm 4.5.

--- tools/onnxruntime/build_migraphx_docker.sh
+++ tools/onnxruntime/build_migraphx_docker.sh
@@ -11,10 +11,10 @@ if [ `id -u` != 0 ]; then
 fi
 cp calc-median miopen37/*.txt miopen37/*.udb miopen37/*.lock onnxruntime/dockerfiles
 cd onnxruntime/dockerfiles
-cp Dockerfile.migraphx Dockerfile.migraphx-rocm43
-echo "RUN apt-get install -y time bc" >> Dockerfile.migraphx-rocm43
-echo "COPY gfx906_60.HIP.2_6_0_8145-rocm-rel-3.7-20-c16087a4.ufdb.txt miopen_1.0.0.udb /root/.config/miopen/" >> Dockerfile.migraphx-rocm43
-echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.migraphx-rocm43
-echo "ENV EXPROVIDER=migraphx" >> Dockerfile.migraphx-rocm43
+cp Dockerfile.migraphx Dockerfile.migraphx-rocm45
+echo "RUN apt-get install -y time bc" >> Dockerfile.migraphx-rocm45
+echo "COPY gfx906_60.HIP.2_6_0_8145-rocm-rel-3.7-20-c16087a4.ufdb.txt miopen_1.0.0.udb /root/.config/miopen/" >> Dockerfile.migraphx-rocm45
+echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.migraphx-rocm45
+echo "ENV EXPROVIDER=migraphx" >> Dockerfile.migraphx-rocm45
 
-docker build ${CACHE} -f Dockerfile.migraphx-rocm43 -t ort:migraphx-rocm43-$DATESTAMP .
+docker build ${CACHE} -f Dockerfile.migraphx-rocm45 -t ort:migraphx-rocm45-$DATESTAMP .
