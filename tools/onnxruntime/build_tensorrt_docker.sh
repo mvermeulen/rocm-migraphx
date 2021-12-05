b52acfcaf58a4fbb3a769b7e05e16d9c3820b63c
Mike Vermeulen <mike@vermeulen.com>
2021-12-05 17:21:50 -0600

    Change location of calc-median.

--- tools/onnxruntime/build_tensorrt_docker.sh
+++ tools/onnxruntime/build_tensorrt_docker.sh
@@ -9,7 +9,7 @@ if [ `id -u` != 0 ]; then
     echo script should be run as root
     exit 0
 fi
-cp calc-median onnxruntime/dockerfiles
+cp calc-median onnxruntime
 cd onnxruntime/dockerfiles
 sed 's/onnxruntime cmake-3.14.3-Linux-x86_64/cmake-3.14.3-Linux-x86_64/g' Dockerfile.tensorrt > Dockerfile.tensorrt-ort
 echo "ENV LD_LIBRARY_PATH=" >> Dockerfile.tensorrt-ort
