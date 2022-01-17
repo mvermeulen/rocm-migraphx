506feda703e4bb65faa1301b5baa503d4afbb4ff
Mike Vermeulen <mike@vermeulen.com>
2022-01-17 07:21:46 -0600

    Fix up latest patch.

--- tools/onnxruntime/cleanup.migx.sh
+++ tools/onnxruntime/cleanup.migx.sh
@@ -2,5 +2,7 @@
 set -x
 rm -rf onnxruntime.save
 mv onnxruntime onnxruntime.save
-git clone --recursive https://github.com/ROCmSoftwarePlatform/onnxruntime
-patch onnxruntime/dockerfiles/Dockerfile.migraphx < patch.migraphx
+git clone --recursive https://github.com/microsoft/onnxruntime
+patch onnxruntime/dockerfiles/Dockerfile.tensorrt < patch.tensorrt 
+patch onnxruntime/dockerfiles/Dockerfile.cuda < patch.cuda
+patch onnxruntime/dockerfiles/Dockerfile.openvino < patch.openvino
