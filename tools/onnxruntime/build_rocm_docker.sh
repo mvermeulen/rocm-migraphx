a1cc210d31d4daab0b0c17a640de911b867b6617
Mike Vermeulen <mike@vermeulen.com>
2022-01-19 07:01:38 -0600

    Add initial autotune example.

--- tools/onnxruntime/build_rocm_docker.sh
+++ tools/onnxruntime/build_rocm_docker.sh
@@ -9,13 +9,12 @@ if [ `id -u` != 0 ]; then
     echo script should be run as root
     exit 0
 fi
-cp calc-median onnxruntime
+cp calc-median onnxruntime/dockerfiles
 cd onnxruntime/dockerfiles
 sed -e 's/--parallel//g' -e 's/rf onnxruntime/rf/g' Dockerfile.rocm > Dockerfile.rocm-ort
 echo "" >> Dockerfile.rocm-ort
 echo "RUN apt update && apt-get install -y time bc" >> Dockerfile.rocm-ort
 echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.rocm-ort
 echo "ENV EXPROVIDER=rocm" >> Dockerfile.rocm-ort
-cd ..
 
-docker build ${CACHE} -f dockerfiles/Dockerfile.rocm-ort -t ort:rocm-$DATESTAMP .
+docker build ${CACHE} -f Dockerfile.rocm-ort -t ort:rocm-$DATESTAMP .
