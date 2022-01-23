560e85ae1bba1e3fa3937a4f8b8f09f348b804a5
Mike Vermeulen <mike@vermeulen.com>
2022-01-23 08:31:14 -0600

    Add MIGraphX to more general cleanup script.

--- tools/onnxruntime/cleanup.sh
+++ tools/onnxruntime/cleanup.sh
@@ -6,3 +6,4 @@ git clone --recursive https://github.com/microsoft/onnxruntime
 patch onnxruntime/dockerfiles/Dockerfile.tensorrt < patch.tensorrt 
 patch onnxruntime/dockerfiles/Dockerfile.cuda < patch.cuda
 patch onnxruntime/dockerfiles/Dockerfile.openvino < patch.openvino
+patch onnxruntime/dockerfiles/Dockerfile.migraphx < patch.migraphx
