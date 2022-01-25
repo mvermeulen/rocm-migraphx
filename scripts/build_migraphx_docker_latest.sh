38ea411b3bfa08f905849dea49f56934218b047f
Mike Vermeulen <mike@vermeulen.com>
2022-01-25 11:03:55 -0600

    Add tools to build branches more easily.

--- scripts/build_migraphx_docker_latest.sh
+++ scripts/build_migraphx_docker_latest.sh
@@ -2,6 +2,8 @@
 set -x
 DATESTAMP=`date '+%Y%m%d'`
 BUILD_NAVI=${BUILD_NAVI:="0"}
+BRANCH=${BRANCH:="develop"}
+DOCKERIMAGE=${DOCKERIMAGE:="rocm-migraphx:$DATESTAMP"}
 
 if [ "$BUILD_NAVI" = "0" ]; then
     BASE=${BASE:="rocm-migraphx:4.5"}
@@ -17,8 +19,8 @@ if [ `id -u` != 0 ]; then
 fi
 cd ../dockerfiles
 if [ "$BUILD_NAVI" = "0" ]; then
-    docker build -t rocm-migraphx:${DATESTAMP} --no-cache --build-arg DOCKERBASE=${BASE} --build-arg MIOPENTUNE=${MIOPENTUNE} -f ../dockerfiles/dockerfile-daily .
+    docker build -t rocm-migraphx:${DATESTAMP} --no-cache --build-arg BRANCH=${BRANCH} --build-arg DOCKERBASE=${BASE} --build-arg MIOPENTUNE=${MIOPENTUNE} -f ../dockerfiles/dockerfile-daily .
 else
-    docker build -t rocm-migraphx:${DATESTAMP}n --no-cache --build-arg DOCKERBASE=${BASE} --build-arg MIOPENTUNE=${MIOPENTUNE} -f ../dockerfiles/dockerfile-daily .
+    docker build -t rocm-migraphx:${DATESTAMP}n --no-cache --build-arg BRANCH=${BRANCH} --build-arg DOCKERBASE=${BASE} --build-arg MIOPENTUNE=${MIOPENTUNE} -f ../dockerfiles/dockerfile-daily .
 fi
 
