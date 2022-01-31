b3e72698c73d29d0e58018f837196b0897588235
Mike Vermeulen <mike@vermeulen.com>
2022-01-30 18:44:10 -0600

    Use MSFT provided dockerfile.

--- tools/onnxruntime/build_rocm_docker.sh
+++ tools/onnxruntime/build_rocm_docker.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 CACHE=${CACHE:="--no-cache"}
 DATESTAMP=`date '+%Y%m%d'`
-if [ ! -f onnxruntime/dockerfiles/Dockerfile.rocm ]; then
-    echo "onnxruntime/dockerfiles/Dockerfile.rocm is missing"
+if [ ! -f Dockerfile.rocm4.5.2.pytorch ]; then
+    echo "Dockerfile.rocm4.5.2.pytorch is missing"
     exit 0
 fi
 if [ `id -u` != 0 ]; then
     echo script should be run as root
     exit 0
 fi
+cp Dockerfile.rocm4.5.2.pytorch onnxruntime/dockerfiles/Dockerfile.rocm-ort
 cp calc-median onnxruntime/dockerfiles
 cd onnxruntime/dockerfiles
-sed -e 's/--parallel//g' -e 's/rf onnxruntime/rf/g' Dockerfile.rocm > Dockerfile.rocm-ort
 echo "" >> Dockerfile.rocm-ort
 echo "RUN apt update && apt-get install -y time bc" >> Dockerfile.rocm-ort
+echo "RUN mkdir /code && ln -s /workspace/github/onnxruntime /code/onnxruntime" >> Dockerfile.rocm-ort
 echo "COPY calc-median /usr/bin/calc-median" >> Dockerfile.rocm-ort
 echo "ENV EXPROVIDER=rocm" >> Dockerfile.rocm-ort
 
