6a1e4d233440af953f8529f11fb1816283ee4a7c
Mike Vermeulen <mike@vermeulen.com>
2022-02-06 11:19:07 -0600

    Remove script, folded into cleanup.sh.

--- tools/onnxruntime/cleanup.migx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-rm -rf onnxruntime.save
-mv onnxruntime onnxruntime.save
-git clone --recursive https://github.com/microsoft/onnxruntime
-patch onnxruntime/dockerfiles/Dockerfile.migraphx < patch.migraphx
