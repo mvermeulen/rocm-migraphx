bb4bdfe007358831eebf40cd4e2f054ddfec1cfe
Mike Vermeulen <mike@vermeulen.com>
2022-02-16 13:37:18 -0600

    Add benchmark runs.

---
+++ scripts/ort_benchmark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This works
+#python3 benchmark.py -g -o no_opt -b  1 2 4 8 16 -m bert-base-cased --sequence_length 128 --precision fp16 --provider rocm
+#python3 benchmark.py -g -o no_opt -b  1 2 4 8 16 -m bert-base-cased --sequence_length 128 --precision fp32 --provider rocm
+
+exit
+# This fails
+python3 benchmark.py -g -o by_ort -b  1 2 4 8 16 -m bert-base-cased --sequence_length 128 --precision fp16 --provider rocm
+python3 benchmark.py -g           -b  1 2 4 8 16 -m bert-base-cased --sequence_length 128 --precision fp16 --provider rocm
+
+# This fails
+python3 profiler.py -g -m onnx_models/bert_base_cased_1.onnx -b 1 -s 128 -g --provider rocm
+
+# This fails (based on develop and migraphx_for_ort)
+python3 benchmark.py -g -o no_opt -b  1 2 4 8 16 -m bert-base-cased --sequence_length 128 --precision fp16 --provider migraphx
