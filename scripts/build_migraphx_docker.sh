be565daeaf19e017106f27800eb563a5e4fed7a4
Mike Vermeulen <mike@vermeulen.com>
2022-03-23 14:17:00 -0500

    Change argument for Navi.

--- scripts/build_migraphx_docker.sh
+++ scripts/build_migraphx_docker.sh
@@ -22,4 +22,4 @@ DOCKERFILE=dockerfile.`date '+%Y-%m-%d'`
 # had difficulties with substitutions
 sed -e "s?ROCM_RELEASE?${ROCM_RELEASE}?g" dockerfile > $DOCKERFILE
 
-docker build --build-arg build_name=${BUILD_NAVI} --build-arg base_image=${ROCM_BASE} --no-cache -t $DOCKERIMAGE -f $DOCKERFILE .
+docker build --build-arg build_navi=${BUILD_NAVI} --build-arg base_image=${ROCM_BASE} --no-cache -t $DOCKERIMAGE -f $DOCKERFILE .
