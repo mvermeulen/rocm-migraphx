2cccd2d2554bf42befdb0bb75bdf08df24ef1778
Mike Vermeulen <mike@vermeulen.com>
2022-04-06 11:05:27 -0500

    Update passing of variables.

--- tools/onnxruntime/ort_benchmark.sh
+++ tools/onnxruntime/ort_benchmark.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -x
 TEST_RESULTDIR=${TEST_RESULTDIR:="/home/mev/source/rocm-migraphx/test-results"}
-EXEPROVIDER=${EXEPROVIDER:="migraphx"}
+EXPROVIDER=${EXPROVIDER:="migraphx"}
 EXEDIR=${EXEDIR:="/workspace/onnxruntime/onnxruntime/python/tools/transformers"}
 
 testdir=${TEST_RESULTDIR}/onnxruntime-`date '+%Y-%m-%d-%H-%M'`
 mkdir $testdir
 pushd $testdir
 git log > $testdir/commit.txt
-case $EXEPROVIDER in
+case $EXPROVIDER in
     cpu)
 	cat /proc/cpuinfo > $testdir/cpu.txt
 	PRECISION="fp32"
@@ -44,16 +44,16 @@ popd
 cd ${EXEDIR}
 touch ${testdir}/summary.csv
 
-echo "\n***** bert-base-cased ${EXEPROVIDER}\n" >> $testdir/dashboard.out
-echo "\n***** bert-base-cased ${EXEPROVIDER}\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-base-cased --sequence_length 32 384 --batch_sizes 1 32 --provider=${EXEPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+echo "\n***** bert-base-cased ${EXPROVIDER}\n" >> $testdir/dashboard.out
+echo "\n***** bert-base-cased ${EXPROVIDER}\n" >> $testdir/dashboard.err
+python benchmark.py -g -m bert-base-cased --sequence_length 32 384 --batch_sizes 1 32 --provider=${EXPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
 echo "\n***** bert-large-uncased migraphx\n" >> $testdir/dashboard.out
 echo "\n***** bert-large-uncased migraphx\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 32 --provider=${EXEPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 32 --provider=${EXPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
 echo "\n***** distilgpt2 migraphx\n" >> $testdir/dashboard.out
 echo "\n***** distilgpt2 migraphx\n" >> $testdir/dashboard.err
-python benchmark.py -g -m distilgpt2 --model_class AutoModelForCausalLM --sequence_length 32 384 --batch_sizes 1 8 --provider=${EXEPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+python benchmark.py -g -m distilgpt2 --model_class AutoModelForCausalLM --sequence_length 32 384 --batch_sizes 1 8 --provider=${EXPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
-cp dashboard.csv ${EXEPROVIDER}.csv
+cp dashboard.csv ${EXPROVIDER}.csv
