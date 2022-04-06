1c1c9937811231e8adf5438907d017eb32200d75
Mike Vermeulen <mike@vermeulen.com>
2022-04-06 12:50:35 -0500

    Change docker reference.

--- tools/onnxruntime/run_ort_migraphx.sh
+++ tools/onnxruntime/run_ort_migraphx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER=${DOCKER:="ort:cuda-20220406"}
+DOCKER=${DOCKER:="ort:migraphx-20220406"}
 
 docker run -e TZ=America/Chicago --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev/:/home/mev ${DOCKER} env EXPROVIDER="migraphx" /home/mev/source/rocm-migraphx/tools/onnxruntime/ort_benchmark.sh
 
