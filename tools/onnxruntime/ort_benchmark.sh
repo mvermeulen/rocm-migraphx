784edc3ff96ad092ba19541b8cb99a9a70cc667b
Mike Vermeulen <mike@vermeulen.com>
2022-04-06 14:24:09 -0500

    Fix up gpuflag.

--- tools/onnxruntime/ort_benchmark.sh
+++ tools/onnxruntime/ort_benchmark.sh
@@ -11,18 +11,21 @@ git log > $testdir/commit.txt
 case $EXPROVIDER in
     cpu)
 	cat /proc/cpuinfo > $testdir/cpu.txt
+	GPUFLAG=""
 	PRECISION="fp32"
 	OPTFLAGS=""
 	;;
     tensorrt)
 	cat /proc/cpuinfo > $testdir/cpu.txt	
 	nvidia-smi -q > $testdir/nvidia.txt
+	GPUFLAG="-g"
 	PRECISION="fp16"	
 	OPTFLAGS=""
 	;;
     cuda)
 	cat /proc/cpuinfo > $testdir/cpu.txt	
 	nvidia-smi -q > $testdir/nvidia.txt
+	GPUFLAG="-g"
 	PRECISION="fp16"	
 	OPTFLAGS=""
 	;;
@@ -30,12 +33,14 @@ case $EXPROVIDER in
 	cat /proc/cpuinfo > $testdir/cpu.txt	
 	export TOKENIZERS_PARALLELISM=false
 	rocminfo > $testdir/rocminfo.txt
+	GPUFLAG="-g"	
 	PRECISION="fp16"
 	OPTFLAGS="--disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu"
 	;;
     rocm)
 	cat /proc/cpuinfo >  $testdir/cpu.txt	
 	rocminfo > $testdir/rocminfo.txt
+	GPUFLAG="-g"	
 	PRECISION="fp16"	
 	OPTFLAGS=""
 	;;
@@ -46,14 +51,14 @@ touch ${testdir}/summary.csv
 
 echo "\n***** bert-base-cased ${EXPROVIDER}\n" >> $testdir/dashboard.out
 echo "\n***** bert-base-cased ${EXPROVIDER}\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-base-cased --sequence_length 32 384 --batch_sizes 1 32 --provider=${EXPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+python benchmark.py $GPUFLAG -m bert-base-cased --sequence_length 32 384 --batch_sizes 1 32 --provider=${EXPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
 echo "\n***** bert-large-uncased migraphx\n" >> $testdir/dashboard.out
 echo "\n***** bert-large-uncased migraphx\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 32 --provider=${EXPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+python benchmark.py $GPUFLAG -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 32 --provider=${EXPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
 echo "\n***** distilgpt2 migraphx\n" >> $testdir/dashboard.out
 echo "\n***** distilgpt2 migraphx\n" >> $testdir/dashboard.err
-python benchmark.py -g -m distilgpt2 --model_class AutoModelForCausalLM --sequence_length 32 384 --batch_sizes 1 8 --provider=${EXPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+python benchmark.py $GPUFLAG -m distilgpt2 --model_class AutoModelForCausalLM --sequence_length 32 384 --batch_sizes 1 8 --provider=${EXPROVIDER} -p ${PRECISION} ${OPTFLAGS} --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
 cp $testdir/dashboard.csv $testdir/${EXPROVIDER}.csv
