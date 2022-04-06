2b83845882a850fb0381313bfedab72dcf184090
Mike Vermeulen <mike@vermeulen.com>
2022-04-06 10:06:51 -0500

    Fix up dockerfile for build.

--- tools/onnxruntime/build_migraphx_docker.sh
+++ tools/onnxruntime/build_migraphx_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DATESTAMP=`date '+%Y%m%d'`
 CACHE=${CACHE:="--no-cache"}
-docker build ${CACHE} -f dockerfile-ort -t ort:migraphx-${DATESTAMP} .
+diff dockerfile-rocm ../../dockerfiles/dockerfile-ort
+docker build ${CACHE} -f dockerfile-rocm -t ort:migraphx-${DATESTAMP} .
