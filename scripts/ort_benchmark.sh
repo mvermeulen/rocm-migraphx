3eb177f60264cbf58415bac668fd23b984c0c196
Mike Vermeulen <mike@vermeulen.com>
2022-04-14 12:53:24 -0500

    Update comment for ideal sequence length/batch.

--- scripts/ort_benchmark.sh
+++ scripts/ort_benchmark.sh
@@ -12,14 +12,20 @@ popd
 cd ${EXEDIR}
 touch ${testdir}/summary.csv
 
+# Ideal batch: 1,2,4,8,16,32,64,128,256
+# Ideal sequence: 16,32,64,128,256,384,512
 echo "\n***** bert-base-cased migraphx\n" >> $testdir/dashboard.out
 echo "\n***** bert-base-cased migraphx\n" >> $testdir/dashboard.err
 python benchmark.py -g -m bert-base-cased --sequence_length 32 384 --batch_sizes 1 32 --provider=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
+# Ideal batch: 1,2,4,8,16,32,64,128
+# Ideal sequence: 16,32,64,128,256,384,512
 echo "\n***** bert-large-uncased migraphx\n" >> $testdir/dashboard.out
 echo "\n***** bert-large-uncased migraphx\n" >> $testdir/dashboard.err
 python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 32 --provider=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
+# Ideal batch: 1,2,4,8,16,32
+# Ideal sequence: 16,32,64,128,256,384,512
 echo "\n***** distilgpt2 migraphx\n" >> $testdir/dashboard.out
 echo "\n***** distilgpt2 migraphx\n" >> $testdir/dashboard.err
 python benchmark.py -g -m distilgpt2 --model_class AutoModelForCausalLM --sequence_length 32 384 --batch_sizes 1 8 --provider=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
