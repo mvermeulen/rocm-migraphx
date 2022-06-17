b163515fd20686cbc301bb7b3e25901f9308b713
Mike Vermeulen <mike@vermeulen.com>
2022-06-17 17:16:57 -0500

    Adjust batch size, and tokenizer messages.

--- scripts/ort_benchmark4.sh
+++ scripts/ort_benchmark4.sh
@@ -2,7 +2,6 @@
 set -x
 TEST_RESULTDIR=${TEST_RESULTDIR:="/home/mev/source/rocm-migraphx/test-results"}
 EXEDIR=${EXEDIR:="/workspace/onnxruntime/onnxruntime/python/tools/transformers"}
-MICROBENCH=${MICROBENCH:="/workspace/onnxruntime/onnxruntime/python/tools/microbench"}
 PYTHONPATH=${PYTHONPATH:="/workspace/migraphx/build/lib"}
 export PYTHONPATH
 testdir=${TEST_RESULTDIR}/onnxruntime-`date '+%Y-%m-%d-%H-%M'`
@@ -23,7 +22,7 @@ python benchmark.py -g -m bert-base-cased --sequence_length 32 384 --batch_sizes
 # Ideal sequence: 16,32,64,128,256,384,512
 echo "\n***** bert-large-uncased migraphx\n" >> $testdir/dashboard.out
 echo "\n***** bert-large-uncased migraphx\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 32 --provider=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-ep.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 16 --provider=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-ep.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
 # Ideal batch: 1,2,4,8,16,32
 # Ideal sequence: 16,32,64,128,256,384,512
@@ -44,7 +43,7 @@ python benchmark.py -g -m bert-base-cased --sequence_length 32 384 --batch_sizes
 # Ideal sequence: 16,32,64,128,256,384,512
 echo "\n***** bert-large-uncased migraphx engine\n" >> $testdir/dashboard.out
 echo "\n***** bert-large-uncased migraphx engine\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 32 --engine=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-eng.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 16 --engine=migraphx -p fp16 --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu --result_csv $testdir/dashboard-eng.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
 # Ideal batch: 1,2,4,8,16,32
 # Ideal sequence: 16,32,64,128,256,384,512
@@ -61,7 +60,7 @@ python benchmark.py -g -m bert-base-cased --sequence_length 32 384 --batch_sizes
 
 echo "\n***** bert-large-uncased rocm\n" >> $testdir/dashboard.out
 echo "\n***** bert-large-uncased rocm\n" >> $testdir/dashboard.err
-python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 32 --provider=rocm -p fp16 --result_csv $testdir/dashboard-rocm.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
+python benchmark.py -g -m bert-large-uncased --sequence_length 32 384 --batch_sizes 1 16 --provider=rocm -p fp16 --result_csv $testdir/dashboard-rocm.csv --detail_csv $testdir/dashboard-detail.csv 1>>$testdir/dashboard.out 2>>$testdir/dashboard.err
 
 echo "\n***** distilgpt2 rocm\n" >> $testdir/dashboard.out
 echo "\n***** distilgpt2 rocm\n" >> $testdir/dashboard.err
