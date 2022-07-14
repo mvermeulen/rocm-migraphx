842fb499f37986f55da5d04aca4d40863324c0a5
Mike Vermeulen <mike@vermeulen.com>
2022-07-14 17:10:04 -0500

    Update shape inference.

--- tools/tuneinfo/shape_infer.py
+++ tools/tuneinfo/shape_infer.py
@@ -1,9 +1,20 @@
-# Example of shape inference
+# Shape inference script
+#
+# Reads an onnx file and creates an updated file with shapes inferred
 #
 import onnx
+import argparse
+import sys
 from onnx import helper,shape_inference
 
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python shape_infer.py input.onnx output.onnx")
+        sys.exit(1)
+    model = onnx.load(sys.argv[1])
+    inferred_model = shape_inference.infer_shapes(model)
+    onnx.save(inferred_model,sys.argv[2])
 
-model = onnx.load("resnet50i1.onnx")
-inferred_model = shape_inference.infer_shapes(model)
-onnx.save(inferred_model,"resnet50i1_inf.onnx")
+if __name__ == "__main__":
+    main()
+    
