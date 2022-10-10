26c33cec1ef7ceb5fd260b7de785ff3a3d35d460
Mike Vermeulen <mike@vermeulen.com>
2022-10-10 16:30:04 -0500

    Update to ROCm 5.3.

--- scripts/build_migraphx_docker_latest.sh
+++ scripts/build_migraphx_docker_latest.sh
@@ -6,9 +6,9 @@ BRANCH=${BRANCH:="develop"}
 DOCKERIMAGE=${DOCKERIMAGE:="rocm-migraphx:$DATESTAMP"}
 
 if [ "$BUILD_NAVI" = "0" ]; then
-    BASE=${BASE:="rocm-migraphx:5.2.3"}
+    BASE=${BASE:="rocm-migraphx:5.3"}
 else
-    BASE=${BASE:="rocm-migraphx:5.2n"}
+    BASE=${BASE:="rocm-migraphx:5.3n"}
 fi
 
 MIOPENTUNE=${MIOPENTUNE:="miopen-rocm52"}
