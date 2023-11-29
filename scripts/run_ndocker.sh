c7ea4b6a19dfbc85d748c27f8dd3b2d2869d1415
Mike Vermeulen <mike@vermeulen.com>
2023-11-29 09:01:52 -0700

    Fix typo.

--- scripts/run_ndocker.sh
+++ scripts/run_ndocker.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-DOCKER=${DOCKER:"cuda-tvm:12"}
+DOCKER=${DOCKER:="cuda-tvm:12"}
 
 if [ `id -u` != 0 ]; then
     echo script should be run as root
     exit 0
 fi
 
-docker run -it -e NVIDIA_DRIVER_CAPABILITIES=compute,graphics,utility --gpus all -v /home/mev:/home/mev \
+docker run -it -e NVIDIA_DRIVER_CAPABILITIES=compute,graphics,utility \
+       --gpus all \
+       -v /home/mev:/home/mev \
        -v /etc/vulkan/icd.d:/etc/vulkan/icd.d \
        -v /usr/share/vulkan/icd.d:/usr/share/vulkan/icd.d \
        -v /etc/glvnd/egl_vendor.d/:/etc/glvnd/egl_vendor.d \
