b0caa3f26163ab2de0dbf441ef2337966b222fdd
Mike Vermeulen <mike@vermeulen.com>
2023-12-08 12:52:34 -0600

    Create new structure including shark.

--- scripts/ort_benchmark5.sh
+++ scripts/ort_benchmark5.sh
@@ -19,6 +19,74 @@ popd
 cd ${EXEDIR}
 touch ${testdir}/summary.csv
 
+ENGINES=('onnxruntime' 'shark' 'torch' 'torch2' 'torchscript' 'tensorflow')
+PROVIDERS=('rocm' 'migraphx' 'cpu')
+
+while read model batch sequence precision
+do
+    file="${model}-b${batch}-s${sequence}-${precision}-${engine}"    
+    for engine in $ENGINES
+    do
+	case $engine in
+	    "onnxruntime")
+		for provider in $PROVIDERS
+		do
+		    tag="${file}-${provider}"
+		    case $provider in
+			"cpu")
+			    options="-e onnxruntime --provider cpu"			    
+			;;
+			"migraphx")
+			    options="-g -e onnxruntime --provider migraphx --disable_gelu --disable_layer_norm --disable_attention --disable_skip_layer_norm --disable_embed_layer_norm --disable_bias_skip_layer_norm --disable_bias_gelu"
+			;;
+			"rocm")
+			    options="-g -e onnxruntime --provider rocm"
+			;;
+		    esac
+		done
+		echo "*** python3 benchmark.py ${options} -m $model --batch_sizes $batch --sequence_length $sequence -p $precision"
+		time python3 benchmark.py ${options} -m $model --batch_sizes $batch --sequence_length $sequence -p $precision --result_csv $testdir/${file}-summary.csv --detail-csv $testdir/${file}-detail-csv 1>$testdir/${tag}.out 2>$testdir/${tag}.err
+		sort -ru ${testdir}/${file}-detail.csv > ${testdir}/${file}-detail-sort.csv
+		sort -ru ${testdir}/${file}-summary.csv > ${testdir}/${file}-summary-sort.csv
+		cat ${testdir}/${file}-summary-sort.csv >> ${testdir}/summary.csv		
+		continue
+		;;
+	    "torch")
+		tag="${file}-${engine}"
+		options="-o no_opt -e torch"		
+		;;
+	    "torch2")
+		tag="${file}-${engine}"
+		options="-o no_opt -e torch2"		
+		;;
+	    "torchscript")
+		tag="${file}-${engine}"
+		options="-o no_opt -e torchscript"		
+		;;
+	    "tensorflow")
+		tag="${file}-${engine}"
+		options="-o no_opt -e tensorflow"		
+		;;
+	    "shark")
+		tag="${file}-${engine}"
+		options="-o no_opt -e shark"
+		;;    
+	esac
+	echo "*** python3 benchmark.py ${options} -m $model --batch_sizes $batch --sequence_length $sequence -p $precision"
+	time python3 benchmark.py ${options} -m $model --batch_sizes $batch --sequence_length $sequence -p $precision --result_csv $testdir/${file}-summary.csv --detail-csv $testdir/${file}-detail-csv 1>$testdir/${tag}.out 2>$testdir/${tag}.err
+	sort -ru ${testdir}/${file}-detail.csv > ${testdir}/${file}-detail-sort.csv
+	sort -ru ${testdir}/${file}-summary.csv > ${testdir}/${file}-summary-sort.csv
+	cat ${testdir}/${file}-summary-sort.csv >> ${testdir}/summary.csv
+    done
+done <<EOF    
+bert-base-uncased 1 128 fp16
+bert-base-uncased 1 128 fp32
+EOF
+
+exit 0
+#**************************************************************************************************************************************************************************
+# Unused...
+#**************************************************************************************************************************************************************************
 # Ideal batch: 1,2,4,8,16,32,64,128,256
 # Ideal sequence: 16,32,64,128,256,384,512
 echo "\n***** bert-base-cased migraphx\n" >> $testdir/dashboard.out
