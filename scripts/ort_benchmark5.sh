c3d8628ed7e30838ef3cc77a6c4c2b55e3738f0c
Mike Vermeulen <mike@vermeulen.com>
2023-12-10 12:50:36 -0600

    Separate out SHARK.

--- scripts/ort_benchmark5.sh
+++ scripts/ort_benchmark5.sh
@@ -95,6 +95,35 @@ distilgpt2 32 32 fp16
 distilgpt2 32 384 fp16
 EOF
 
+# Run SHARK separately because need to set up the environment
+while read model batch sequence precision
+do
+    file="${model}-b${batch}-s${sequence}-${precision}"    
+    tag="${file}-${engine}"
+    options="-g -o no_opt -e shark"
+    echo "*** python3 benchmark.py ${options} -m $model --batch_sizes $batch --sequence_length $sequence -p $precision"
+    /usr/bin/time -o $testdir/${tag}.time python3 benchmark.py ${options} -m $model --batch_sizes $batch --sequence_length $sequence -p $precision --result_csv $testdir/${file}-shark-summary.csv --detail_csv $testdir/${file}-shark-detail.csv 1>$testdir/${tag}.out 2>$testdir/${tag}.err
+    sort -ru ${testdir}/${file}-shark-detail.csv > ${testdir}/${file}-detail-sort.csv
+    sort -ru ${testdir}/${file}-shark-summary.csv > ${testdir}/${file}-summary-sort.csv
+    cat ${testdir}/${file}-shark-summary-sort.csv >> ${testdir}/shark-summary.csv
+done <<EOF    
+bert-base-uncased 1 128 fp16
+bert-base-uncased 1 128 fp32
+bert-base-cased 1 32 fp16
+bert-base-cased 1 384 fp16
+bert-base-cased 32 32 fp16
+bert-base-cased 32 384 fp16
+bert-large-uncased 1 32 fp16
+bert-large-uncased 1 384 fp16
+bert-large-uncased 32 32 fp16
+bert-large-uncased 32 384 fp16
+distilgpt2 1 32 fp16
+distilgpt2 1 384 fp16
+distilgpt2 32 32 fp16
+distilgpt2 32 384 fp16
+EOF
+
+
 exit 0
 # Previous models of interest
 #bert-base-cased 1 32 fp16
