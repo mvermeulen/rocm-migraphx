9e4eb0b65581554426c5f4fd707ef5f5b56fada3
Mike Vermeulen <mike@vermeulen.com>
2023-12-11 11:59:22 -0600

    Add script to run docker.

---
+++ perf-compare/scripts/run_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DOCKER=${DOCKER:="ort-migraphx:5.7.2"}
+
+if [ `id -u` != 0 ]; then
+    echo script should be run as root
+    exit 0
+fi
+
+if [ -d /extra ]; then
+    EXTRAMOUNT="-v /extra:/extra"
+else
+    EXTRAMOUNT=""
+fi
+
+docker run -it -e TZ=America/Chicago -e TARGET=gpu --device=/dev/dri --device=/dev/kfd --network=host --group-add=video -v /home/mev:/home/mev $EXTRAMOUNT $DOCKER /bin/bash
