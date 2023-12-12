10145ebcf290b4b6863fabcc6a86b85ba8e62ae3
Mike Vermeulen <mike@vermeulen.com>
2023-12-11 19:10:18 -0600

    Add logfiles.

--- perf-compare/docker/build_all.sh
+++ perf-compare/docker/build_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TAG=${TAG:="5.7.2"}
+DATESTAMP=`date '+%Y%m%d-%H%M'`
 
-docker build -f rocm-dev -t rocm:${TAG} .
-docker build -f migraphx-dev -t migraphx:${TAG} --build-arg base_docker=rocm:${TAG} .
-docker build -f ort-migraphx-dev -t ort-migraphx:${TAG} --build-arg base_docker=migraphx:${TAG} .
+docker build -f rocm-dev -t rocm:${TAG} . 2>&1 | tee rocm:${TAG}.${DATESTAMP}.log
+docker build -f migraphx-dev -t migraphx:${TAG} --build-arg base_docker=rocm:${TAG} . 2>&1 | tee migraphx:${TAG}.${DATESTAMP}.log
+docker build -f ort-migraphx-dev -t ort-migraphx:${TAG} --build-arg base_docker=migraphx:${TAG} . 2>&1 | tee ort-migraphx:${TAG}.${DATESTAMP}.log
