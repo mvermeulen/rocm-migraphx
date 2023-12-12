c4e9b9362f335e480e5be2de0bfb14eff7fb4c80
Mike Vermeulen <mike@vermeulen.com>
2023-12-11 18:39:42 -0600

    Remove the -dev name for docker container.

--- perf-compare/docker/build_all.sh
+++ perf-compare/docker/build_all.sh
@@ -2,5 +2,5 @@
 TAG=${TAG:="5.7.2"}
 
 docker build -f rocm-dev -t rocm-dev:${TAG} .
-docker build -f migraphx-dev -t migraphx-dev:${TAG} --build-arg base_docker=rocm-dev:${TAG} .
-docker build -f ort-migraphx-dev -t ort-migraphx:${TAG} --build-arg base_docker=migraphx-dev:${TAG} .
+docker build -f migraphx-dev -t migraphx:${TAG} --build-arg base_docker=rocm-dev:${TAG} .
+docker build -f ort-migraphx-dev -t ort-migraphx:${TAG} --build-arg base_docker=migraphx:${TAG} .
